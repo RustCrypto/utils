35e706dec87d5777ded727197bf3437e1be50f3f
Артём Павлов [Artyom Pavlov] <newpavlov@gmail.com>
2017-05-14 18:01:46 +0300

    Updated MAC tests

--- hmac/tests/lib.rs
+++ hmac/tests/lib.rs
@@ -1,52 +1,57 @@
-// TODO: move HMAC tests to the hash meta-crate
+#![no_std]
+#[macro_use]
+extern crate crypto_tests;
 extern crate hmac;
 extern crate md_5 as md5;
+extern crate sha2;
 
-use hmac::{Mac, MacResult, Hmac};
-
-pub struct MacTest {
-    pub name: &'static str,
-    pub key: &'static [u8],
-    pub input: &'static [u8],
-    pub output: &'static [u8],
-}
-
-macro_rules! new_tests {
-    ( $( $name:expr ),*  ) => {
-        [$(
-            MacTest {
-                name: $name,
-                key: include_bytes!(concat!("data/", $name, ".key.bin")),
-                input: include_bytes!(concat!("data/", $name, ".input.bin")),
-                output: include_bytes!(concat!("data/", $name, ".output.bin")),
-            },
-        )*]
-    };
-}
+use crypto_tests::mac::{mac_test, MacTest};
+use hmac::Hmac;
 
 #[test]
 fn hmac_md5() {
-    // Test vectors from: http://tools.ietf.org/html/rfc2104
-    // Plus wiki test
-    let tests = new_tests!("1", "2", "3", "4");
-    for test in tests.iter() {
-        let mut hmac = Hmac::<md5::Md5>::new(test.key);
-        hmac.input(&test.input[..]);
-        let result = hmac.result();
-        let expected = MacResult::new_from_slice(test.output);
-        println!("result: {:?}", result.code());
-        println!("expected: {:?}", expected.code());
-        assert!(result == expected);
-    }
+    // Test vectors from RFC 2104, plus wiki test
+    let tests = new_mac_tests!("md5/1", "md5/2", "md5/3", "md5/4");
+    mac_test::<Hmac<md5::Md5>>(&tests);
+}
 
-    // incremental test
-    for test in tests.iter() {
-        let mut hmac = Hmac::<md5::Md5>::new(test.key);
-        for i in 0..test.input.len() {
-            hmac.input(&test.input[i..i + 1]);
-        }
-        let result = hmac.result();
-        let expected = MacResult::new_from_slice(test.output);
-        assert!(result == expected);
-    }
+
+#[test]
+fn hmac_sha224() {
+    // Test vectors from RFC 4231
+    let tests = new_mac_tests!(
+        "sha224/1", "sha224/2", "sha224/3", "sha224/4",
+        "sha224/5", "sha224/6", "sha224/7"
+    );
+    mac_test::<Hmac<sha2::Sha224>>(&tests);
+}
+
+#[test]
+fn hmac_sha256() {
+    // Test vectors from RFC 4231
+    let tests = new_mac_tests!(
+        "sha256/1", "sha256/2", "sha256/3", "sha256/4",
+        "sha256/5", "sha256/6", "sha256/7"
+    );
+    mac_test::<Hmac<sha2::Sha256>>(&tests);
+}
+
+#[test]
+fn hmac_sha384() {
+    // Test vectors from RFC 4231
+    let tests = new_mac_tests!(
+        "sha384/1", "sha384/2", "sha384/3", "sha384/4",
+        "sha384/5", "sha384/6", "sha384/7"
+    );
+    mac_test::<Hmac<sha2::Sha384>>(&tests);
+}
+
+#[test]
+fn hmac_sha512() {
+    // Test vectors from RFC 4231
+    let tests = new_mac_tests!(
+        "sha512/1", "sha512/2", "sha512/3", "sha512/4",
+        "sha512/5", "sha512/6", "sha512/7"
+    );
+    mac_test::<Hmac<sha2::Sha512>>(&tests);
 }
