12b85f9a7f1f59fe835cef2d22a5015f3df9fd18
Artyom Pavlov <newpavlov@gmail.com>
2017-06-06 14:24:48 +0300

    Byte tools update

---
+++ byte-tools/src/write_slice.rs
@@ -0,0 +1,44 @@
+use core::ptr;
+
+macro_rules! write_slice {
+    ($src:expr, $dst:expr, $size:expr, $which:ident) => ({
+        assert_eq!($dst.len(), $size*$src.len());
+        unsafe {
+            ptr::copy_nonoverlapping(
+                $src.as_ptr() as *const u8,
+                $dst.as_mut_ptr(),
+                $dst.len());
+        }
+        for v in $dst.iter_mut() {
+            *v = v.$which();
+        }
+    });
+}
+
+/// Write a vector of u32s into a vector of bytes. The values are written in
+/// little-endian format.
+#[inline]
+pub fn write_u32v_le(dst: &mut [u8], src: &[u32]) {
+    write_slice!(src, dst, 4, to_le);
+}
+
+/// Write a vector of u32s into a vector of bytes. The values are written in
+/// big-endian format.
+#[inline]
+pub fn write_u32v_be(dst: &mut [u8], src: &[u32]) {
+    write_slice!(src, dst, 4, to_be);
+}
+
+/// Write a vector of u64s into a vector of bytes. The values are written in
+/// little-endian format.
+#[inline]
+pub fn write_u64v_le(dst: &mut [u8], src: &[u64]) {
+    write_slice!(src, dst, 8, to_le);
+}
+
+/// Write a vector of u64s into a vector of bytes. The values are written in
+/// little-endian format.
+#[inline]
+pub fn write_u64v_be(dst: &mut [u8], src: &[u64]) {
+    write_slice!(src, dst, 8, to_be);
+}
