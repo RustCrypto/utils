d3e78d27f2abce9790fe154178325bea3b1eb94a
Artyom Pavlov <newpavlov@gmail.com>
2017-06-12 04:05:39 +0300

    Block buffer update

--- block-buffer/src/paddings.rs
+++ block-buffer/src/paddings.rs
@@ -1,17 +1,48 @@
 use byte_tools::{zero, set};
 
+/// Trait for padding messages divided into blocks
 pub trait Padding {
+    /// Pads `block` filled with data up to `pos`
     fn pad(block: &mut [u8], pos: usize);
 }
 
-#[derive(Copy, Clone, Debug)]
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+/// Error for indicating failed unpadding process
 pub struct UnpadError;
 
+/// Trait for extracting oringinal message from padded medium
 pub trait Unpadding {
+    /// Unpad given `data` by truncating it according to the used padding.
+    /// In case of the malformed padding will return `UnpadError`
     fn unpad(data: &[u8]) -> Result<&[u8], UnpadError>;
 }
 
-#[derive(Copy, Clone, Debug)]
+
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+pub enum ZeroPadding{}
+
+impl Padding for ZeroPadding {
+    #[inline]
+    fn pad(block: &mut [u8], pos: usize) {
+        zero(&mut block[pos..])
+    }
+}
+
+impl Unpadding for ZeroPadding {
+    #[inline]
+    fn unpad(data: &[u8]) -> Result<&[u8], UnpadError> {
+        let mut n = data.len() - 1;
+        while n != 0 {
+            if data[n] != 0 {
+                break;
+            }
+            n -= 1;
+        }
+        Ok(&data[..n+1])
+    }
+}
+
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub enum Pkcs7{}
 
 impl Padding for Pkcs7 {
@@ -38,31 +69,7 @@ impl Unpadding for Pkcs7 {
     }
 }
 
-#[derive(Copy, Clone, Debug)]
-pub enum ZeroPadding{}
-
-impl Padding for ZeroPadding {
-    #[inline]
-    fn pad(block: &mut [u8], pos: usize) {
-        zero(&mut block[pos..])
-    }
-}
-
-impl Unpadding for ZeroPadding {
-    #[inline]
-    fn unpad(data: &[u8]) -> Result<&[u8], UnpadError> {
-        let mut n = data.len() - 1;
-        while n != 0 {
-            if data[n] != 0 {
-                break;
-            }
-            n -= 1;
-        }
-        Ok(&data[..n+1])
-    }
-}
-
-#[derive(Copy, Clone, Debug)]
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub enum AnsiX923{}
 
 impl Padding for AnsiX923 {
@@ -92,7 +99,7 @@ impl Unpadding for AnsiX923 {
 
 
 
-#[derive(Copy, Clone, Debug)]
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub enum Iso7816{}
 
 impl Padding for Iso7816 {
