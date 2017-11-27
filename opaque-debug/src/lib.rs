f9303506e496c6cb26d9ecc922487d91a6347b57
newpavlov <newpavlov@gmail.com>
2017-11-27 13:57:18 +0300

    opaque-debug v0.2.0

--- opaque-debug/src/lib.rs
+++ opaque-debug/src/lib.rs
@@ -1,18 +1,26 @@
 //! Macro for opaque `Debug` trait implementation.
 #![no_std]
 
-/// Macro for defining opaque `Debug` implementation. It will use the following
-/// format: "HasherName { ... }". While it's convinient to have it
-/// (e.g. for including in other structs), it could be undesirable to leak
-/// internall state, which can happen for example through uncareful logging.
+/// Macro for defining opaque `Debug` implementation.
+///
+/// It will use the following format: "StructName { ... }". While it's
+/// convinient to have it (e.g. for including into other structs), it could be
+/// undesirable to leak internall state, which can happen for example through
+/// uncareful logging.
 #[macro_export]
 macro_rules! impl_opaque_debug {
-    ($state:ty) => {
-        use core::fmt;
-
-        impl fmt::Debug for $state {
+    ($struct:ty) => {
+        #[cfg(feature = "std")]
+        impl ::std::fmt::Debug for $struct {
             fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
-                write!(f, concat!(stringify!($state), " {{ ... }}"))
+                write!(f, concat!(stringify!($struct), " {{ ... }}"))
+            }
+        }
+
+        #[cfg(not(feature = "std"))]
+        impl ::core::fmt::Debug for $struct {
+            fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+                write!(f, concat!(stringify!($struct), " {{ ... }}"))
             }
         }
     }
