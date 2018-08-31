706f32e0381a8ce065138880c367a696dd55602e
newpavlov <newpavlov@gmail.com>
2018-08-31 14:47:01 +0300

    two blobby iterators

--- blobby/src/lib.rs
+++ blobby/src/lib.rs
@@ -4,32 +4,32 @@ extern crate byteorder;
 use byteorder::{LE, ByteOrder};
 use core::iter::{Iterator, FusedIterator, ExactSizeIterator};
 
-pub struct BlobIterator<'a> {
+pub struct DupBlobIterator<'a> {
     index: &'a [u8],
     data: &'a [u8],
     pos: usize,
 }
 
-impl<'a> BlobIterator<'a> {
+impl<'a> DupBlobIterator<'a> {
     pub fn new(data: &'a [u8]) -> Result<Self, &'static str> {
-        let (magic, data) = data.split_at(6);
-        if magic != b"blobby" { Err("invalid data prefix")? }
-        let (len, data) = data.split_at(2);
-        let len = LE::read_u16(len) as usize;
+        let (magic, data) = data.split_at(4);
+        if magic != b"BLBD" { Err("invalid data prefix")? }
+        let (len, data) = data.split_at(4);
+        let len = LE::read_u32(len) as usize;
         let (index, data) = data.split_at(8*len);
         for chunk in index.chunks(8) {
             let start = LE::read_u32(&chunk[..4]) as usize;
             let end = LE::read_u32(&chunk[4..]) as usize;
             if start > end { Err("index: start is bigger than end")? }
             if end > data.len() {
-                Err("index: end points outside of data index")?
+                Err("index: end points outside of data segment")?
             }
         }
         Ok(Self { index, data, pos: 0 })
     }
 }
 
-impl<'a> Iterator for BlobIterator<'a> {
+impl<'a> Iterator for DupBlobIterator<'a> {
     type Item = &'a [u8];
 
     fn next(&mut self) -> Option<&'a [u8]> {
@@ -47,8 +47,57 @@ impl<'a> Iterator for BlobIterator<'a> {
     }
 }
 
-impl<'a> FusedIterator for BlobIterator<'a> { }
+impl<'a> FusedIterator for DupBlobIterator<'a> { }
 
-impl<'a> ExactSizeIterator for BlobIterator<'a> {
+impl<'a> ExactSizeIterator for DupBlobIterator<'a> {
     fn len(&self) -> usize { self.index.len()/8 - self.pos }
+}
+
+pub struct UniqueBlobIterator<'a> {
+    index: &'a [u8],
+    data: &'a [u8],
+    pos: usize,
+    cursor: usize,
+}
+
+impl<'a> UniqueBlobIterator<'a> {
+    pub fn new(data: &'a [u8]) -> Result<Self, &'static str> {
+        let (magic, data) = data.split_at(4);
+        if magic != b"BLBU" { Err("invalid data prefix")? }
+        let (len, data) = data.split_at(4);
+        let len = LE::read_u32(len) as usize;
+        let (index, data) = data.split_at(4*len);
+        let sum = index.chunks(4)
+            .map(LE::read_u32)
+            .fold(0, |a, v| a + v as usize);
+        if sum > data.len() {
+            Err("index element points outside of data segment")?
+        }
+        Ok(Self { index, data, pos: 0, cursor: 0 })
+    }
+}
+
+impl<'a> Iterator for UniqueBlobIterator<'a> {
+    type Item = &'a [u8];
+
+    fn next(&mut self) -> Option<&'a [u8]> {
+        if self.pos >= self.index.len()/4 { return None; }
+        let n = 4*self.pos;
+        let len = LE::read_u32(&self.index[n..n + 4]) as usize;
+        let start = self.cursor;
+        self.pos += 1;
+        self.cursor += len;
+        Some(&self.data[start..self.cursor])
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        let n = self.index.len()/4 - self.pos;
+        (n, Some(n))
+    }
+}
+
+impl<'a> FusedIterator for UniqueBlobIterator<'a> { }
+
+impl<'a> ExactSizeIterator for UniqueBlobIterator<'a> {
+    fn len(&self) -> usize { self.index.len()/4 - self.pos }
 }
--- blobby/tests/mod.rs
+++ blobby/tests/mod.rs
@@ -1,8 +1,20 @@
 #![no_std]
 extern crate blobby;
 
-fn check(data: &[u8], result: &[&[u8]]) {
-    let mut blob = blobby::BlobIterator::new(data).unwrap();
+fn check_dup(data: &[u8], result: &[&[u8]]) {
+    let mut blob = blobby::DupBlobIterator::new(data).unwrap();
+    let mut res = result.iter();
+    loop {
+        match (blob.next(), res.next()) {
+            (Some(v1), Some(v2)) => assert_eq!(&v1, v2),
+            (None, None) => break,
+            _ => panic!("items number mismatch"),
+        }
+    }
+}
+
+fn check_unique(data: &[u8], result: &[&[u8]]) {
+    let mut blob = blobby::UniqueBlobIterator::new(data).unwrap();
     let mut res = result.iter();
     loop {
         match (blob.next(), res.next()) {
@@ -14,29 +26,70 @@ fn check(data: &[u8], result: &[&[u8]]) {
 }
 
 #[test]
-fn empty() {
-    let data = b"blobby\x00\x00";
-    check(data, &[]);
+fn dup_empty() {
+    let data = b"BLBD\x00\x00\x00\x00";
+    check_dup(data, &[]);
 }
 
 #[test]
-fn single() {
-    let data = b"blobby\
-        \x01\x00\
+fn dup_single() {
+    let data = b"BLBD\
+        \x01\x00\x00\x00\
         \x00\x00\x00\x00\x0A\x00\x00\x00\
         0123456789\
     ";
-    check(data, &[b"0123456789"]);
+    check_dup(data, &[b"0123456789"]);
 }
 
 #[test]
-fn double() {
-    let data = b"blobby\
-        \x02\x00\
+fn dup_double() {
+    let data = b"BLBD\
+        \x02\x00\x00\x00\
         \x00\x00\x00\x00\x0A\x00\x00\x00\
         \x0A\x00\x00\x00\x0D\x00\x00\x00\
         0123456789\
         abc\
     ";
-    check(data, &[b"0123456789", b"abc"]);
+    check_dup(data, &[b"0123456789", b"abc"]);
+}
+
+#[test]
+fn dup_dublicate() {
+    let data = b"BLBD\
+        \x03\x00\x00\x00\
+        \x00\x00\x00\x00\x0A\x00\x00\x00\
+        \x0A\x00\x00\x00\x0D\x00\x00\x00\
+        \x00\x00\x00\x00\x0A\x00\x00\x00\
+        0123456789\
+        abc\
+    ";
+    check_dup(data, &[b"0123456789", b"abc", b"0123456789"]);
+}
+
+#[test]
+fn unique_empty() {
+    let data = b"BLBU\x00\x00\x00\x00";
+    check_unique(data, &[]);
+}
+
+#[test]
+fn unique_single() {
+    let data = b"BLBU\
+        \x01\x00\x00\x00\
+        \x0A\x00\x00\x00\
+        0123456789\
+    ";
+    check_unique(data, &[b"0123456789"]);
+}
+
+#[test]
+fn unique_double() {
+    let data = b"BLBU\
+        \x02\x00\x00\x00\
+        \x0A\x00\x00\x00\
+        \x03\x00\x00\x00\
+        0123456789\
+        abc\
+    ";
+    check_unique(data, &[b"0123456789", b"abc"]);
 }
