70bcfb25c650326b44befee3156e9b31ff77acfd
Tony Arcieri <bascule@gmail.com>
2020-08-04 08:55:48 -0700

    const-oid validation
    Attempting to use the new `const fn` features which shipped in 1.46.
    
    This uses a hack to give an "invalid OID" message at compile time,
    however it'd be nice to use a proper panic message when `const_panic`
    stabilizes:
    
    https://github.com/rust-lang/rust/issues/51999

--- const-oid/src/lib.rs
+++ const-oid/src/lib.rs
@@ -5,6 +5,18 @@
 //!
 //! It has full `no_std` support with no dependencies on a heap/liballoc and
 //! stores OID values as static data.
+//!
+//! # About OIDs
+//!
+//! Object Identifiers, a.k.a. OIDs, are an International Telecommunications Union (ITU) and
+//! ISO/IEC standard for naming any object, concept, or "thing" with a globally unambiguous
+//! persistent name.
+//!
+//! See also: https://en.wikipedia.org/wiki/Object_identifier
+//!
+//! # Minimum Supported Rust Version
+//!
+//! This crate requires **Rust 1.46** at a minimum.
 
 #![no_std]
 #![doc(
@@ -27,7 +39,25 @@ pub struct ObjectIdentifier {
 
 impl ObjectIdentifier {
     /// Create a new OID
+    #[allow(clippy::no_effect)]
     pub const fn new(nodes: &'static [u32]) -> Self {
+        // TODO(tarcieri): replace this with const panic when OIDs are invalid
+        // See: https://github.com/rust-lang/rust/issues/51999
+        let mut is_invalid = nodes.len() <= 2;
+
+        match nodes[0] {
+            0..=2 => (),
+            _ => is_invalid = true,
+        }
+
+        match nodes[1] {
+            0..=39 => {}
+            _ => is_invalid = true,
+        }
+
+        // TODO(tarcieri): better error message
+        ["invalid OID"][is_invalid as usize];
+
         Self { nodes }
     }
 }
@@ -63,5 +93,23 @@ mod tests {
     fn display_test() {
         let oid = EXAMPLE_OID.to_string();
         assert_eq!(oid, "1.2.840.10045.3.1.7");
+    }
+
+    #[test]
+    #[should_panic]
+    fn truncated_oid_test() {
+        ObjectIdentifier::new(&[1, 2]);
+    }
+
+    #[test]
+    #[should_panic]
+    fn invalid_first_node_test() {
+        ObjectIdentifier::new(&[3, 2, 840, 10045, 3, 1, 7]);
+    }
+
+    #[test]
+    #[should_panic]
+    fn invalid_second_node_test() {
+        ObjectIdentifier::new(&[1, 40, 840, 10045, 3, 1, 7]);
     }
 }
