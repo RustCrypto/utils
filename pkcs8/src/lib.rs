a9ebedcd9be0af538c9aef319d2025fa607e9566
Tony Arcieri <bascule@gmail.com>
2020-12-04 10:05:32 -0800

    pkcs8: extract PrivateKeyInfo into module (#92)

--- pkcs8/src/lib.rs
+++ pkcs8/src/lib.rs
@@ -37,6 +37,7 @@ extern crate std;
 mod algorithm;
 mod asn1;
 mod error;
+mod private_key_info;
 
 #[cfg(feature = "pem")]
 #[cfg_attr(docsrs, doc(cfg(feature = "pem")))]
@@ -45,53 +46,6 @@ pub mod pem;
 pub use crate::{
     algorithm::AlgorithmIdentifier,
     error::{Error, Result},
+    private_key_info::PrivateKeyInfo,
 };
 pub use const_oid::ObjectIdentifier;
-
-use core::convert::TryFrom;
-
-/// PKCS#8 `PrivateKeyInfo`.
-///
-/// ASN.1 structure containing an [`AlgorithmIdentifier`] and private key
-/// data in an algorithm specific format.
-///
-/// Described in RFC 5208 Section 5:
-/// <https://tools.ietf.org/html/rfc5208#section-5>
-///
-/// ```text
-/// PrivateKeyInfo ::= SEQUENCE {
-///         version                   Version,
-///         privateKeyAlgorithm       PrivateKeyAlgorithmIdentifier,
-///         privateKey                PrivateKey,
-///         attributes           [0]  IMPLICIT Attributes OPTIONAL }
-///
-/// Version ::= INTEGER
-///
-/// PrivateKeyAlgorithmIdentifier ::= AlgorithmIdentifier
-///
-/// PrivateKey ::= OCTET STRING
-///
-/// Attributes ::= SET OF Attribute
-/// ```
-pub struct PrivateKeyInfo<'a> {
-    /// X.509 [`AlgorithmIdentifier`]
-    pub algorithm: AlgorithmIdentifier,
-
-    /// Private key data
-    pub private_key: &'a [u8],
-}
-
-impl<'a> PrivateKeyInfo<'a> {
-    /// Parse [`PrivateKeyInfo`] encoded as ASN.1 DER
-    pub fn from_der(bytes: &'a [u8]) -> Result<Self> {
-        asn1::parse_private_key_info(bytes)
-    }
-}
-
-impl<'a> TryFrom<&'a [u8]> for PrivateKeyInfo<'a> {
-    type Error = Error;
-
-    fn try_from(bytes: &'a [u8]) -> Result<Self> {
-        Self::from_der(bytes)
-    }
-}
---
+++ pkcs8/src/private_key_info.rs
@@ -0,0 +1,50 @@
+//! PKCS#8 `PrivateKeyInfo`.
+
+use crate::{asn1, AlgorithmIdentifier, Error, Result};
+use core::convert::TryFrom;
+
+/// PKCS#8 `PrivateKeyInfo`.
+///
+/// ASN.1 structure containing an [`AlgorithmIdentifier`] and private key
+/// data in an algorithm specific format.
+///
+/// Described in RFC 5208 Section 5:
+/// <https://tools.ietf.org/html/rfc5208#section-5>
+///
+/// ```text
+/// PrivateKeyInfo ::= SEQUENCE {
+///         version                   Version,
+///         privateKeyAlgorithm       PrivateKeyAlgorithmIdentifier,
+///         privateKey                PrivateKey,
+///         attributes           [0]  IMPLICIT Attributes OPTIONAL }
+///
+/// Version ::= INTEGER
+///
+/// PrivateKeyAlgorithmIdentifier ::= AlgorithmIdentifier
+///
+/// PrivateKey ::= OCTET STRING
+///
+/// Attributes ::= SET OF Attribute
+/// ```
+pub struct PrivateKeyInfo<'a> {
+    /// X.509 [`AlgorithmIdentifier`]
+    pub algorithm: AlgorithmIdentifier,
+
+    /// Private key data
+    pub private_key: &'a [u8],
+}
+
+impl<'a> PrivateKeyInfo<'a> {
+    /// Parse [`PrivateKeyInfo`] encoded as ASN.1 DER
+    pub fn from_der(bytes: &'a [u8]) -> Result<Self> {
+        asn1::parse_private_key_info(bytes)
+    }
+}
+
+impl<'a> TryFrom<&'a [u8]> for PrivateKeyInfo<'a> {
+    type Error = Error;
+
+    fn try_from(bytes: &'a [u8]) -> Result<Self> {
+        Self::from_der(bytes)
+    }
+}
