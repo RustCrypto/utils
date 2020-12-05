ab4ed83ad0af8f4650a76e017aa298b336e8aba4
Tony Arcieri <bascule@gmail.com>
2020-12-05 11:15:33 -0800

    const-oid v0.3.3 (#111)

--- const-oid/src/lib.rs
+++ const-oid/src/lib.rs
@@ -35,7 +35,7 @@
 #![no_std]
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo_small.png",
-    html_root_url = "https://docs.rs/const-oid/0.3.2"
+    html_root_url = "https://docs.rs/const-oid/0.3.3"
 )]
 #![forbid(unsafe_code)]
 #![warn(missing_docs, rust_2018_idioms)]
