134d3eb76cf9412f35182c3a80d7e58704e340e7
Tony Arcieri <bascule@gmail.com>
2021-01-10 17:02:08 -0800

    der v0.2.0-pre

--- der/src/lib.rs
+++ der/src/lib.rs
@@ -305,7 +305,7 @@
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo_small.png",
-    html_root_url = "https://docs.rs/der/0.1.0"
+    html_root_url = "https://docs.rs/der/0.2.0-pre"
 )]
 #![forbid(unsafe_code)]
 #![warn(missing_docs, rust_2018_idioms)]
