39ef46c61f4658e9dc2a4cd91aa7cda0f8397adc
Artyom Pavlov <newpavlov@gmail.com>
2021-01-20 10:31:46 +0000

    block-buffer: mark digest_pad as public

--- block-buffer/src/lib.rs
+++ block-buffer/src/lib.rs
@@ -174,7 +174,7 @@ impl<BlockSize: ArrayLength<u8>> BlockBuffer<BlockSize> {
     /// the `suffix` bytes. If there is not enough unused space, `compress`
     /// will be called twice.
     #[inline(always)]
-    fn digest_pad(&mut self, suffix: &[u8], mut compress: impl FnMut(&Block<BlockSize>)) {
+    pub fn digest_pad(&mut self, suffix: &[u8], mut compress: impl FnMut(&Block<BlockSize>)) {
         let pos = self.get_pos();
         self.buffer[pos] = 0x80;
         for b in &mut self.buffer[pos + 1..] {
