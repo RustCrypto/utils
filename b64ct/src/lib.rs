6dd55d491b08c8532016e45cdbb9be71a7db2f00
Tony Arcieri <bascule@gmail.com>
2021-01-19 17:45:26 -0800

    b64ct v0.2.0

--- b64ct/src/lib.rs
+++ b64ct/src/lib.rs
@@ -68,7 +68,7 @@
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg",
     html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg",
-    html_root_url = "https://docs.rs/b64ct/0.1.0"
+    html_root_url = "https://docs.rs/b64ct/0.2.0"
 )]
 #![warn(missing_docs, rust_2018_idioms)]
 
