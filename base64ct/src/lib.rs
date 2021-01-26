cb04f173ebf9b76d46993ed35fe915a9cd9e5330
Tony Arcieri <bascule@gmail.com>
2021-01-26 11:15:44 -0800

    base64ct v0.1.0

--- base64ct/src/lib.rs
+++ base64ct/src/lib.rs
@@ -1,9 +1,66 @@
-//! Pure Rust implementation of Base64 encoding ([RFC 4648, section 4]).
+//! Pure Rust implementation of Base64 encoding ([RFC 4648, section 4])
+//! with a constant-time `no_std`-friendly implementation.
+//!
+//! # About
+//!
+//! This crate implements the following Base64 variants in constant-time:
+//!
+//! - Standard Base64 encoding: `[A-Za-z0-9+/]`
+//!   - [`base64ct::padded`][`padded`]
+//!   - [`base64ct::unpadded`][`unpadded`]
+//! - URL-safe Base64: `[A-Za-z0-9\-_]`
+//!   - [`base64ct::url::padded`][`url::padded`]
+//!   - [`base64ct::url::unpadded`][`url::unpadded`]
+//!
+//! The padded variants require (`=`) padding. Unpadded variants expressly
+//! reject such padding.
+//!
+//! Whitespace is expressly disallowed.
+//!
+//! # Usage
+//!
+//! ## Allocating (enable `alloc` crate feature)
+//!
+//! ```
+//! # #[cfg(feature = "alloc")]
+//! # {
+//! use base64ct::padded as base64;
+//!
+//! let bytes = b"example bytestring!";
+//! let encoded = base64::encode_string(bytes);
+//! assert_eq!(encoded, "ZXhhbXBsZSBieXRlc3RyaW5nIQ==");
+//!
+//! let decoded = base64::decode_vec(&encoded).unwrap();
+//! assert_eq!(decoded, bytes);
+//! # }
+//! ```
+//!
+//! ## Heapless `no_std` usage
+//!
+//! ```
+//! use base64ct::padded as base64;
+//!
+//! const BUF_SIZE: usize = 128;
+//!
+//! let bytes = b"example bytestring!";
+//! assert!(base64::encoded_len(bytes) <= BUF_SIZE);
+//!
+//! let mut enc_buf = [0u8; BUF_SIZE];
+//! let encoded = base64::encode(bytes, &mut enc_buf).unwrap();
+//! assert_eq!(encoded, "ZXhhbXBsZSBieXRlc3RyaW5nIQ==");
+//!
+//! let mut dec_buf = [0u8; BUF_SIZE];
+//! let decoded = base64::decode(encoded, &mut dec_buf).unwrap();
+//! assert_eq!(decoded, bytes);
+//! ```
 //!
 //! # Implementation
 //!
-//! Implemented without data-dependent branches or lookup tables, thereby
-//! providing portable "best effort" constant-time operation.
+//! Implemented using bitwise arithmetic alone without any lookup tables or
+//! data-dependent branches, thereby providing portable "best effort"
+//! constant-time operation.
+//!
+//! Not constant-time with respect to message length (only data).
 //!
 //! Adapted from the following constant-time C++ implementation of Base64:
 //!
@@ -19,7 +76,7 @@
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg",
     html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg",
-    html_root_url = "https://docs.rs/base64ct/0.0.0"
+    html_root_url = "https://docs.rs/base64ct/0.1.0"
 )]
 #![warn(missing_docs, rust_2018_idioms)]
 
