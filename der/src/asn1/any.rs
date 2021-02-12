550493768421ab5030c97047882b24ef44204f04
Tony Arcieri <bascule@gmail.com>
2021-02-12 15:23:26 -0800

    der: add notes about ASN.1 ANY type

--- der/src/asn1/any.rs
+++ der/src/asn1/any.rs
@@ -9,7 +9,15 @@ use core::convert::{TryFrom, TryInto};
 #[cfg(feature = "oid")]
 use crate::ObjectIdentifier;
 
-/// ASN.1 `ANY` type: represents any explicitly tagged ASN.1 value.
+/// ASN.1 `ANY`: represents any explicitly tagged ASN.1 value.
+///
+/// Technically `ANY` hasn't been a recommended part of ASN.1 since the X.209
+/// revision from 1988. It was deprecated and replaced by Information Object
+/// Classes in X.680 in 1994, and X.690 no longer refers to it whatsoever.
+///
+/// Nevertheless, this crate defines an [`Any`] type as it remains a familiar
+/// and useful concept, although the usage within arguably resembles the
+/// type system concept more than the original ASN.1 concept.
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
 pub struct Any<'a> {
     /// Tag representing the type of the encoded value
