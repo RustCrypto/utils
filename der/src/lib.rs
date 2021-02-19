d3acaee1943134a7ee1bf948f09c4f756b948609
Tony Arcieri <bascule@gmail.com>
2021-02-19 08:08:45 -0800

    der v0.2.6 (#282)

--- der/src/lib.rs
+++ der/src/lib.rs
@@ -317,7 +317,7 @@
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg",
     html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg",
-    html_root_url = "https://docs.rs/der/0.2.5"
+    html_root_url = "https://docs.rs/der/0.2.6"
 )]
 #![forbid(unsafe_code)]
 #![warn(missing_docs, rust_2018_idioms, unused_qualifications)]
