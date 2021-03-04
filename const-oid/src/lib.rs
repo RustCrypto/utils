724c077621ec541e3800ce4b254d1b55e76318e7
Tony Arcieri <bascule@gmail.com>
2021-03-04 05:38:27 -0800

    const-oid v0.4.5 (#324)

--- const-oid/src/lib.rs
+++ const-oid/src/lib.rs
@@ -37,7 +37,7 @@
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg",
     html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg",
-    html_root_url = "https://docs.rs/const-oid/0.4.4"
+    html_root_url = "https://docs.rs/const-oid/0.4.5"
 )]
 #![forbid(unsafe_code)]
 #![warn(missing_docs, rust_2018_idioms)]
