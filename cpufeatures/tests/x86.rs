695c899d2fbafc4db7c9450f2369c5d66f73ebdb
Tony Arcieri <bascule@gmail.com>
2021-05-13 09:45:37 -0700

    cpufeatures: test NEON (#404)
    Adds `neon` to the armcaps used in the test

--- cpufeatures/tests/x86.rs
+++ cpufeatures/tests/x86.rs
@@ -2,16 +2,16 @@
 
 #![cfg(any(target_arch = "x86", target_arch = "x86_64"))]
 
-cpufeatures::new!(cpuid_aes_sha, "aes", "sha");
+cpufeatures::new!(cpuid, "aes", "sha");
 
 #[test]
 fn init() {
-    let token: cpuid_aes_sha::InitToken = cpuid_aes_sha::init();
-    assert_eq!(token.get(), cpuid_aes_sha::get());
+    let token: cpuid::InitToken = cpuid::init();
+    assert_eq!(token.get(), cpuid::get());
 }
 
 #[test]
 fn init_get() {
-    let (token, val) = cpuid_aes_sha::init_get();
+    let (token, val) = cpuid::init_get();
     assert_eq!(val, token.get());
 }
