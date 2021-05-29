99a7ae40d8136c61e3a2fcb4a5b33f1c28ceb984
Tony Arcieri <bascule@gmail.com>
2021-05-28 19:17:01 -0700

    crypto-bigint: optional `generic-array` interop
    There is one interop strategy that is possible between `generic-array`
    and const generics today, and it's similar to the one already used by
    `typenum` (and, as it were, `crypto-bigint` itself): avoid any sort of
    const-generic parameterization when dealing with traits, and provide an
    impl of a trait for each desired size, with a macro to write them.
    
    This commit adds a (currently decode-only) `ArrayEncoding` trait with a
    `ByteSize` parameter which must be an `ArrayLength<u8>`.
    
    It uses the macros for defining type aliases for `UInt` to add an impl
    of this new trait.
    
    As it were, `typenum` doesn't support all of the sizes that were
    previously defined, however it seems like the sizes that `typenum` does
    support are actually the sensible ones for a cryptographic bignum
    library, so this commit also matches the `U*` sizes with the ones
    provided by typenum.

--- crypto-bigint/src/lib.rs
+++ crypto-bigint/src/lib.rs
@@ -22,9 +22,18 @@ mod ops;
 mod traits;
 mod uint;
 
+#[cfg(feature = "generic-array")]
+mod array;
+
 pub use crate::{
     traits::{NumBits, NumBytes},
     uint::*,
+};
+
+#[cfg(feature = "generic-array")]
+pub use {
+    self::array::{ArrayEncoding, ByteArray},
+    generic_array::{self, typenum::consts},
 };
 
 /// Big integers modeled as an array of smaller integers called "limbs"
