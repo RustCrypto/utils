2eff0f34b62539c4b1b9c994bff69f6db14f4dd6
Tony Arcieri <bascule@gmail.com>
2021-05-30 11:30:39 -0700

    crypto-bigint: add `nlimbs!` macro
    Adds a macro which can compute the number of limbs required to store an
    n-bit number at compile time.
    
    This is intended as a stop gap until a forthcoming Rust feature like
    `const_evaluatable_checked` allows it to be replaced with something such
    as a `const fn`.
    
    For now it's needed to compute various const generic parameters however,
    so a macro is the only option available.

--- crypto-bigint/src/macros.rs
+++ crypto-bigint/src/macros.rs
@@ -8,3 +8,33 @@ macro_rules! const_assert {
         [$msg][!$bool as usize]
     };
 }
+
+/// Calculate the number of limbs required to represent the given number of bits.
+// TODO(tarcieri): replace with `const_evaluatable_checked` (e.g. a `const fn`) when stable
+#[macro_export]
+macro_rules! nlimbs {
+    ($bits:expr) => {
+        $bits / 8 / $crate::LIMB_BYTES
+    };
+}
+
+#[cfg(test)]
+mod tests {
+    #[cfg(target_pointer_width = "32")]
+    #[test]
+    fn nlimbs_for_bits_macro() {
+        assert_eq!(nlimbs!(64), 2);
+        assert_eq!(nlimbs!(128), 4);
+        assert_eq!(nlimbs!(192), 6);
+        assert_eq!(nlimbs!(256), 8);
+    }
+
+    #[cfg(target_pointer_width = "64")]
+    #[test]
+    fn nlimbs_for_bits_macro() {
+        assert_eq!(nlimbs!(64), 1);
+        assert_eq!(nlimbs!(128), 2);
+        assert_eq!(nlimbs!(192), 3);
+        assert_eq!(nlimbs!(256), 4);
+    }
+}
