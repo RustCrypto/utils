a791f68f940eedd0ce81451a86ca678ea0177422
Tony Arcieri <bascule@gmail.com>
2021-05-30 09:33:29 -0700

    crypto-bigint: subtraction support (#445)
    Implements `sbb`, `wrapping_sub`, and `checked_sub` functions on `UInt`.

--- crypto-bigint/src/limb.rs
+++ crypto-bigint/src/limb.rs
@@ -26,6 +26,24 @@ pub(crate) const fn adc(a: Limb, b: Limb, carry: Limb) -> (Limb, Limb) {
     (ret as u64, (ret >> 64) as u64)
 }
 
+/// Computes `a - (b + borrow)`, returning the result along with the new borrow.
+/// 32-bit version.
+#[cfg(target_pointer_width = "32")]
+#[inline(always)]
+pub const fn sbb(a: Limb, b: Limb, borrow: Limb) -> (Limb, Limb) {
+    let ret = (a as u64).wrapping_sub((b as u64) + ((borrow >> 31) as u64));
+    (ret as u32, (ret >> 32) as u32)
+}
+
+/// Computes `a - (b + borrow)`, returning the result along with the new borrow.
+/// 64-bit version.
+#[cfg(target_pointer_width = "64")]
+#[inline(always)]
+pub const fn sbb(a: Limb, b: Limb, borrow: Limb) -> (Limb, Limb) {
+    let ret = (a as u128).wrapping_sub((b as u128) + ((borrow >> 63) as u128));
+    (ret as u64, (ret >> 64) as u64)
+}
+
 /// Computes `a + (b * c) + carry`, returning the result along with the new carry.
 /// 32-bit version.
 #[cfg(target_pointer_width = "32")]
