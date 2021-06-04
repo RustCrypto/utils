0d335463e3875626ee87378bd7031d790e08a0bd
Tony Arcieri <bascule@gmail.com>
2021-06-03 23:17:24 -0700

    der: const generic signed integer decoder with i32/i64/i128 support
    This PR is a corollary to #469, but implements signed integer decoding

--- der/src/asn1/integer/uint.rs
+++ der/src/asn1/integer/uint.rs
@@ -42,8 +42,8 @@ pub(crate) fn decode<const N: usize>(any: Any<'_>) -> Result<[u8; N]> {
 }
 
 /// Encode the given big endian bytes representing an integer as ASN.1 DER.
-pub(crate) fn encode(encoder: &mut Encoder<'_>, bytes: &[u8]) -> Result<()> {
-    let bytes = strip_leading_zeroes(bytes);
+pub(crate) fn encode<const N: usize>(encoder: &mut Encoder<'_>, bytes: [u8; N]) -> Result<()> {
+    let bytes = strip_leading_zeroes(&bytes);
     let leading_zero = needs_leading_zero(bytes);
     let len = (Length::try_from(bytes.len())? + leading_zero as u8)?;
     Header::new(Tag::Integer, len)?.encode(encoder)?;
@@ -57,14 +57,13 @@ pub(crate) fn encode(encoder: &mut Encoder<'_>, bytes: &[u8]) -> Result<()> {
 
 /// Get the encoded length for the given unsigned integer serialized as bytes.
 #[inline]
-pub(crate) fn encoded_len(bytes: &[u8]) -> Result<Length> {
-    let bytes = strip_leading_zeroes(bytes);
+pub(crate) fn encoded_len<const N: usize>(bytes: [u8; N]) -> Result<Length> {
+    let bytes = strip_leading_zeroes(&bytes);
     Length::try_from(bytes.len())? + needs_leading_zero(bytes) as u8
 }
 
 /// Strip the leading zeroes from the given byte slice
 fn strip_leading_zeroes(mut bytes: &[u8]) -> &[u8] {
-    // Strip leading zeroes
     while let Some((byte, rest)) = bytes.split_first() {
         if *byte == 0 && !rest.is_empty() {
             bytes = rest;
