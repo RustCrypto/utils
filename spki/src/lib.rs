5bddebc68260a76daeb5d4be8ce515ac5c460736
Tony Arcieri <bascule@gmail.com>
2021-06-07 19:13:30 -0700

    spki v0.4.0 (#491)

--- spki/src/lib.rs
+++ spki/src/lib.rs
@@ -33,7 +33,7 @@
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg",
     html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg",
-    html_root_url = "https://docs.rs/spki/0.4.0-pre"
+    html_root_url = "https://docs.rs/spki/0.4.0"
 )]
 #![forbid(unsafe_code, clippy::unwrap_used)]
 #![warn(missing_docs, rust_2018_idioms, unused_qualifications)]
