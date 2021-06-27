8579815e996c6b74d74e176cdffee3b6a2974722
dignifiedquire <me@dignifiedquire.com>
2021-06-27 19:24:52 +0200

    convert add_mod and neg_mod to generic code

--- crypto-bigint/src/uint/add_mod.rs
+++ crypto-bigint/src/uint/add_mod.rs
@@ -42,29 +42,33 @@ pub(super) mod base {
     }
 
     pub const fn add2(a: &UInt<2>, b: &UInt<2>, p: &UInt<2>) -> UInt<2> {
-        let (l0, carry) = a.limbs[0].adc(b.limbs[0], Limb::ZERO);
-        let (l1, _) = a.limbs[1].adc(b.limbs[1], carry);
-
-        // Subtract the modulus, to ensure the result is smaller.
-        UInt::new([l0, l1]).sub_mod(p, p)
+        add(a, b, p)
     }
 
     pub const fn add3(a: &UInt<3>, b: &UInt<3>, p: &UInt<3>) -> UInt<3> {
-        let (l0, carry) = a.limbs[0].adc(b.limbs[0], Limb::ZERO);
-        let (l1, carry) = a.limbs[1].adc(b.limbs[1], carry);
-        let (l2, _) = a.limbs[2].adc(b.limbs[2], carry);
-
-        // Subtract the modulus, to ensure the result is smaller.
-        UInt::new([l0, l1, l2]).sub_mod(p, p)
+        add(a, b, p)
     }
 
     pub const fn add4(a: &UInt<4>, b: &UInt<4>, p: &UInt<4>) -> UInt<4> {
-        let (l0, carry) = a.limbs[0].adc(b.limbs[0], Limb::ZERO);
-        let (l1, carry) = a.limbs[1].adc(b.limbs[1], carry);
-        let (l2, carry) = a.limbs[2].adc(b.limbs[2], carry);
-        let (l3, _) = a.limbs[3].adc(b.limbs[3], carry);
+        add(a, b, p)
+    }
+
+    pub const fn add<const LIMBS: usize>(
+        a: &UInt<LIMBS>,
+        b: &UInt<LIMBS>,
+        p: &UInt<LIMBS>,
+    ) -> UInt<LIMBS> {
+        let mut out = [Limb::ZERO; LIMBS];
+        let mut carry = Limb::ZERO;
+        let mut i = 0;
+        while i < LIMBS {
+            let (l, c) = a.limbs[i].adc(b.limbs[i], carry);
+            out[i] = l;
+            carry = c;
+            i += 1;
+        }
 
         // Subtract the modulus, to ensure the result is smaller.
-        UInt::new([l0, l1, l2, l3]).sub_mod(p, p)
+        super::super::sub_mod::base::sub(&UInt::new(out), p, p)
     }
 }
--- crypto-bigint/src/uint/neg_mod.rs
+++ crypto-bigint/src/uint/neg_mod.rs
@@ -5,9 +5,7 @@ use super::UInt;
 impl UInt<1> {
     /// Computes `-a mod p` in constant time.
     pub const fn neg_mod(&self, p: &Self) -> Self {
-        let l0 = base::neg1(self.limbs[0], p.limbs[0]);
-
-        UInt::new([l0])
+        base::neg1(self, p)
     }
 }
 
@@ -35,103 +33,65 @@ impl UInt<4> {
 pub(super) mod base {
     use crate::{Limb, UInt};
 
-    pub const fn neg1(a: Limb, p: Limb) -> Limb {
-        // Subtract `a` from `p` to negate. Ignore the final
-        // borrow because it cannot underflow; a is guaranteed to
-        // be in the field.
-        let (l0, _) = p.sbb(a, Limb::ZERO);
-
-        // `tmp` could be `p` if `a` was zero. Create a mask that is
-        // zero if `a` was zero, and `Limb::MAX` if self was nonzero.
-
-        // FIXME: constant time comparison
-        let v = if a.eq_vartime(&Limb::ZERO) {
-            Limb::ONE
-        } else {
-            Limb::ZERO
-        };
-        let mask = v.wrapping_sub(Limb::ONE);
-
-        l0.bitand(mask)
+    pub const fn neg1(a: &UInt<1>, p: &UInt<1>) -> UInt<1> {
+        neg(a, p)
     }
 
     pub const fn neg2(a: &UInt<2>, p: &UInt<2>) -> UInt<2> {
-        // Subtract `a` from `p` to negate. Ignore the final
-        // borrow because it cannot underflow; a is guaranteed to
-        // be in the field.
-        let (l0, borrow) = p.limbs[0].sbb(a.limbs[0], Limb::ZERO);
-        let (l1, _) = p.limbs[1].sbb(a.limbs[1], borrow);
-
-        // `tmp` could be `p` if `a` was zero. Create a mask that is
-        // zero if `a` was zero, and `Limb::MAX` if self was nonzero.
-
-        // FIXME: constant time comparison
-        let v = if a.limbs[0].bitor(a.limbs[1]).eq_vartime(&Limb::ZERO) {
-            Limb::ONE
-        } else {
-            Limb::ZERO
-        };
-        let mask = v.wrapping_sub(Limb::ONE);
-
-        UInt::new([l0.bitand(mask), l1.bitand(mask)])
+        neg(a, p)
     }
 
     pub const fn neg3(a: &UInt<3>, p: &UInt<3>) -> UInt<3> {
-        // Subtract `a` from `p` to negate. Ignore the final
-        // borrow because it cannot underflow; a is guaranteed to
-        // be in the field.
-        let (l0, borrow) = p.limbs[0].sbb(a.limbs[0], Limb::ZERO);
-        let (l1, borrow) = p.limbs[1].sbb(a.limbs[1], borrow);
-        let (l2, _) = p.limbs[2].sbb(a.limbs[2], borrow);
-
-        // `tmp` could be `p` if `a` was zero. Create a mask that is
-        // zero if `a` was zero, and `Limb::MAX` if self was nonzero.
-
-        // FIXME: constant time comparison
-        let v = if a.limbs[0]
-            .bitor(a.limbs[1])
-            .bitor(a.limbs[2])
-            .eq_vartime(&Limb::ZERO)
-        {
-            Limb::ONE
-        } else {
-            Limb::ZERO
-        };
-        let mask = v.wrapping_sub(Limb::ONE);
-
-        UInt::new([l0.bitand(mask), l1.bitand(mask), l2.bitand(mask)])
+        neg(a, p)
     }
 
     pub const fn neg4(a: &UInt<4>, p: &UInt<4>) -> UInt<4> {
+        neg(a, p)
+    }
+
+    pub const fn neg<const LIMBS: usize>(a: &UInt<LIMBS>, p: &UInt<LIMBS>) -> UInt<LIMBS> {
+        let mut tmp = [Limb::ZERO; LIMBS];
+
         // Subtract `a` from `p` to negate. Ignore the final
         // borrow because it cannot underflow; a is guaranteed to
         // be in the field.
-        let (l0, borrow) = p.limbs[0].sbb(a.limbs[0], Limb::ZERO);
-        let (l1, borrow) = p.limbs[1].sbb(a.limbs[1], borrow);
-        let (l2, borrow) = p.limbs[2].sbb(a.limbs[2], borrow);
-        let (l3, _) = p.limbs[3].sbb(a.limbs[3], borrow);
+
+        let mut borrow = Limb::ZERO;
+        let mut i = 0;
+        while i < LIMBS {
+            let (l, b) = p.limbs[i].sbb(a.limbs[i], borrow);
+            tmp[i] = l;
+            borrow = b;
+
+            i += 1;
+        }
 
         // `tmp` could be `p` if `a` was zero. Create a mask that is
         // zero if `a` was zero, and `Limb::MAX` if self was nonzero.
 
         // FIXME: constant time comparison
-        let v = if a.limbs[0]
-            .bitor(a.limbs[1])
-            .bitor(a.limbs[2])
-            .bitor(a.limbs[3])
-            .eq_vartime(&Limb::ZERO)
-        {
+        let mut a_or = a.limbs[0];
+        let mut i = 1;
+        while i < LIMBS {
+            a_or = a_or.bitor(a.limbs[i]);
+
+            i += 1;
+        }
+
+        let v = if a_or.eq_vartime(&Limb::ZERO) {
             Limb::ONE
         } else {
             Limb::ZERO
         };
         let mask = v.wrapping_sub(Limb::ONE);
 
-        UInt::new([
-            l0.bitand(mask),
-            l1.bitand(mask),
-            l2.bitand(mask),
-            l3.bitand(mask),
-        ])
+        let mut i = 0;
+        while i < LIMBS {
+            tmp[i] = tmp[i].bitand(mask);
+
+            i += 1;
+        }
+
+        UInt::new(tmp)
     }
 }
