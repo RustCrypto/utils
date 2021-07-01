be5ccf10e5321e897390c457e1a0c4f4983b1fb5
dignifiedquire <me@dignifiedquire.com>
2021-07-01 19:49:44 +0200

    test: implement basic tests for add_mod and sub_mod

--- crypto-bigint/src/uint/add_mod.rs
+++ crypto-bigint/src/uint/add_mod.rs
@@ -3,28 +3,80 @@
 use super::UInt;
 
 macro_rules! impl_add_mod {
-    ($size:expr, $base_name:ident) => {
+    ($size:expr, $base_name:ident, $test_name:ident) => {
         impl UInt<$size> {
             /// Computes `a + b mod p` in constant time.
+            ///
+            /// Assumes `a` and `b` are `< p`.
             pub const fn add_mod(&self, rhs: &Self, p: &Self) -> Self {
                 base::$base_name(&self, &rhs, &p)
+            }
+        }
+
+        #[cfg(all(test, feature = "rand"))]
+        #[test]
+        fn $test_name() {
+            use crate::Limb;
+            use rand_core::SeedableRng;
+
+            let mut rng = rand_chacha::ChaCha8Rng::seed_from_u64(1);
+
+            let moduli = [UInt::<$size>::random(&mut rng), UInt::random(&mut rng)];
+
+            for p in &moduli {
+                let base_cases = [(1u64, 0u64, 1u64), (0, 1, 1), (0, 0, 0)];
+                for (a, b, c) in &base_cases {
+                    let a: UInt<$size> = (*a).into();
+                    let b: UInt<$size> = (*b).into();
+                    let c: UInt<$size> = (*c).into();
+
+                    assert_eq!(c, a.add_mod(&b, p));
+                }
+
+                assert_eq!(p.add_mod(&0u64.into(), p), 0u64.into());
+                assert_eq!(p.add_mod(&1u64.into(), p), 1u64.into());
+
+                if $size > 1 {
+                    for _i in 0..100 {
+                        let a: UInt<$size> = Limb::random(&mut rng).into();
+                        let b: UInt<$size> = Limb::random(&mut rng).into();
+                        let (a, b) = if a < b { (b, a) } else { (a, b) };
+
+                        let c = a.add_mod(&b, p);
+                        assert!(c < *p, "not reduced");
+                        assert_eq!(c, a.wrapping_add(&b), "result incorrect");
+                    }
+                }
+
+                for _i in 0..100 {
+                    let a = UInt::<$size>::random_mod(&mut rng, p);
+                    let b = UInt::<$size>::random_mod(&mut rng, p);
+
+                    let c = a.add_mod(&b, p);
+                    assert!(c < *p, "not reduced: {} >= {} ", c, p);
+
+                    let x = a.wrapping_add(&b);
+                    if x < *p {
+                        assert_eq!(c, x, "incorrect result");
+                    }
+                }
             }
         }
     };
 }
 
-impl_add_mod!(1, add1);
-impl_add_mod!(2, add2);
-impl_add_mod!(3, add3);
-impl_add_mod!(4, add4);
-impl_add_mod!(5, add5);
-impl_add_mod!(6, add6);
-impl_add_mod!(7, add7);
-impl_add_mod!(8, add8);
-impl_add_mod!(9, add9);
-impl_add_mod!(10, add10);
-impl_add_mod!(11, add11);
-impl_add_mod!(12, add12);
+impl_add_mod!(1, add1, test_add1);
+impl_add_mod!(2, add2, test_add2);
+impl_add_mod!(3, add3, test_add3);
+impl_add_mod!(4, add4, test_add4);
+impl_add_mod!(5, add5, test_add5);
+impl_add_mod!(6, add6, test_add6);
+impl_add_mod!(7, add7, test_add7);
+impl_add_mod!(8, add8, test_add8);
+impl_add_mod!(9, add9, test_add9);
+impl_add_mod!(10, add10, test_add10);
+impl_add_mod!(11, add11, test_add11);
+impl_add_mod!(12, add12, test_add12);
 
 pub(super) mod base {
     use crate::{Limb, UInt};
--- crypto-bigint/src/uint/sub_mod.rs
+++ crypto-bigint/src/uint/sub_mod.rs
@@ -3,28 +3,81 @@
 use super::UInt;
 
 macro_rules! impl_sub_mod {
-    ($size:expr, $base_name:ident) => {
+    ($size:expr, $base_name:ident, $test_name:ident) => {
         impl UInt<$size> {
             /// Computes `a - b mod p` in constant time.
+            ///
+            /// Assumes `a` and `b` are `< p`.
             pub const fn sub_mod(&self, rhs: &Self, p: &Self) -> Self {
                 base::$base_name(self, rhs, p)
+            }
+        }
+
+        #[cfg(all(test, feature = "rand"))]
+        #[test]
+        fn $test_name() {
+            use crate::Limb;
+            use rand_core::SeedableRng;
+
+            let mut rng = rand_chacha::ChaCha8Rng::seed_from_u64(1);
+
+            let moduli = [UInt::<$size>::random(&mut rng), UInt::random(&mut rng)];
+
+            for p in &moduli {
+                let base_cases = [
+                    (1u64, 0u64, 1u64.into()),
+                    (0, 1, p.wrapping_sub(&1u64.into())),
+                    (0, 0, 0u64.into()),
+                ];
+                for (a, b, c) in &base_cases {
+                    let a: UInt<$size> = (*a).into();
+                    let b: UInt<$size> = (*b).into();
+
+                    let x = a.sub_mod(&b, p);
+                    assert_eq!(*c, x, "{} - {} mod {} = {} != {}", a, b, p, x, c);
+                }
+
+                if $size > 1 {
+                    for _i in 0..100 {
+                        let a: UInt<$size> = Limb::random(&mut rng).into();
+                        let b: UInt<$size> = Limb::random(&mut rng).into();
+                        let (a, b) = if a < b { (b, a) } else { (a, b) };
+
+                        let c = a.sub_mod(&b, p);
+                        assert!(c < *p, "not reduced");
+                        assert_eq!(c, a.wrapping_sub(&b), "result incorrect");
+                    }
+                }
+
+                for _i in 0..100 {
+                    let a = UInt::<$size>::random_mod(&mut rng, p);
+                    let b = UInt::<$size>::random_mod(&mut rng, p);
+
+                    let c = a.sub_mod(&b, p);
+                    assert!(c < *p, "not reduced: {} >= {} ", c, p);
+
+                    let x = a.wrapping_sub(&b);
+                    if a >= b && x < *p {
+                        assert_eq!(c, x, "incorrect result");
+                    }
+                }
             }
         }
     };
 }
 
-impl_sub_mod!(1, sub1);
-impl_sub_mod!(2, sub2);
-impl_sub_mod!(3, sub3);
-impl_sub_mod!(4, sub4);
-impl_sub_mod!(5, sub5);
-impl_sub_mod!(6, sub6);
-impl_sub_mod!(7, sub7);
-impl_sub_mod!(8, sub8);
-impl_sub_mod!(9, sub9);
-impl_sub_mod!(10, sub10);
-impl_sub_mod!(11, sub11);
-impl_sub_mod!(12, sub12);
+impl_sub_mod!(1, sub1, test_sub1);
+impl_sub_mod!(2, sub2, test_sub2);
+impl_sub_mod!(3, sub3, test_sub3);
+impl_sub_mod!(4, sub4, test_sub4);
+impl_sub_mod!(5, sub5, test_sub5);
+impl_sub_mod!(6, sub6, test_sub6);
+impl_sub_mod!(7, sub7, test_sub7);
+impl_sub_mod!(8, sub8, test_sub8);
+impl_sub_mod!(9, sub9, test_sub9);
+impl_sub_mod!(10, sub10, test_sub10);
+impl_sub_mod!(11, sub11, test_sub11);
+impl_sub_mod!(12, sub12, test_sub12);
 
 pub(super) mod base {
     use crate::{Limb, UInt};
