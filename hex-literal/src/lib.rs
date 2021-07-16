06a70c527ba89794a42957ff298914fcc0c5b59b
Артём Павлов [Artyom Pavlov] <newpavlov@gmail.com>
2021-07-16 11:45:46 +0300

    bump version and update changelog

--- hex-literal/src/lib.rs
+++ hex-literal/src/lib.rs
@@ -46,7 +46,7 @@
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg",
     html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg",
-    html_root_url = "https://docs.rs/hex-literal/0.3.2"
+    html_root_url = "https://docs.rs/hex-literal/0.3.3"
 )]
 
 mod comments;
