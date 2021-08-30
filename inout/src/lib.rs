c4168330d7a97db5b24ff16c32b7c6c686ac4048
Артём Павлов <newpavlov@gmail.com>
2021-08-30 15:16:04 +0300

    inout: add crate-level attributes

--- inout/src/lib.rs
+++ inout/src/lib.rs
@@ -1,5 +1,11 @@
 #![no_std]
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/RustCrypto/media/8f1a9894/logo.svg",
+    html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/media/8f1a9894/logo.svg"
+)]
 #![allow(clippy::needless_lifetimes)]
+#![warn(missing_docs, rust_2018_idioms)]
+
 use core::{convert::TryInto, marker::PhantomData, slice};
 use generic_array::{ArrayLength, GenericArray};
 
