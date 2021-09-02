8f38cb53cd040b2faba544507812a23545930797
Tony Arcieri <bascule@gmail.com>
2021-09-02 12:37:40 -0600

    crypto-bigint v0.2.5

--- crypto-bigint/src/lib.rs
+++ crypto-bigint/src/lib.rs
@@ -35,7 +35,7 @@
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg",
     html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg",
-    html_root_url = "https://docs.rs/crypto-bigint/0.2.4"
+    html_root_url = "https://docs.rs/crypto-bigint/0.2.5"
 )]
 #![forbid(unsafe_code, clippy::unwrap_used)]
 #![warn(missing_docs, rust_2018_idioms, unused_qualifications)]
