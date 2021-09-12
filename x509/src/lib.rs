e1f5248c244f3e61b8cd030135cb3c88a35afab4
Tony Arcieri <bascule@gmail.com>
2021-09-12 11:26:38 -0600

    x509: `cargo audit` workaround
    This is effectively a bug in `cargo audit` where it sees a yanked `x509`
    crate published to crates.io and confuses it with the workspace member.
    
    To work around the issue, this commit bumps the version of the `x509`
    crate in the workspace to v0.0.1, which hasn't been yanked.

--- x509/src/lib.rs
+++ x509/src/lib.rs
@@ -8,7 +8,7 @@
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg",
     html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg",
-    html_root_url = "https://docs.rs/x509/0.0.0"
+    html_root_url = "https://docs.rs/x509/0.0.1"
 )]
 #![forbid(unsafe_code)]
 #![warn(missing_docs, rust_2018_idioms)]
