0d16377d32415f8988fb7c39c6b4e6afe318a178
Tony Arcieri <bascule@gmail.com>
2021-09-14 09:07:56 -0600

    Move format-related crates to RustCrypto/formats (#636)
    Splits out PKCS and X.509-related crates into a new repo:
    
    https://github.com/RustCrypto/formats/pull/2
    
    This PR leaves a directory for each of these crates in-place with a
    README.md pointing to the new location. We can eventually remove these,
    perhaps sometime after we've published new versions of all of the
    format-related crates which point to the new location.

--- pkcs8/src/document.rs
+++
@@ -1,7 +0,0 @@
-//! Serialized DER-encoded documents stored in heap-backed buffers.
-// TODO(tarcieri): heapless support?
-
-#[cfg(feature = "pkcs5")]
-pub(crate) mod encrypted_private_key;
-pub(crate) mod private_key;
-pub(crate) mod public_key;
