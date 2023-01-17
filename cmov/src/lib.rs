73110e18ba1cbe9bac524e8a3d6a17a64bdaa7e7
Tony Arcieri <bascule@gmail.com>
2023-01-16 22:06:42 -0700

    cmov: redesigned trait-based API
    Replaces the existing API with a `Cmov` trait which is impl'd for all of
    the unsigned integer types.
    
    On x86 platforms this adds support for using both EAX and RAX register
    operands.
    
    On aarch64 platforms it adds support for using both W0 and X0 register
    operands.
    
    Support for `u8` is impl'd generically via upcasting, and support for
    `u128` is impl'd generically by splitting the value into two `u64`s and
    doing two selections.
    
    Platform-by-platform support is now split into `aarch64`, `x86`, and
    `portable` backend modules.

--- cmov/src/lib.rs
+++ cmov/src/lib.rs
@@ -6,146 +6,69 @@
 )]
 #![warn(missing_docs, rust_2018_idioms, unused_qualifications)]
 
-#[cfg(any(target_arch = "x86", target_arch = "x86_64", target_arch = "aarch64"))]
-use core::arch::asm;
-
-/// Move if zero.
-///
-/// Uses a `test` instruction to check if the given `condition` value is
-/// equal to zero, then calls `cmovz` (a.k.a. `cmove`) to conditionally move
-/// `src` to `dst` when `condition` is equal to zero.
+#[cfg(target_arch = "aarch64")]
+mod aarch64;
+#[cfg(not(any(target_arch = "aarch64", target_arch = "x86", target_arch = "x86_64")))]
+mod portable;
 #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
-#[inline(always)]
-pub fn cmovz(condition: usize, src: usize, dst: &mut usize) {
-    unsafe {
-        asm! {
-            "test {0}, {0}",
-            "cmovz {1}, {2}",
-            in(reg) condition,
-            inlateout(reg) *dst,
-            in(reg) src,
-            options(pure, nomem, nostack),
-        };
+mod x86;
+
+/// Condition
+pub type Condition = u8;
+
+/// Conditional move
+pub trait Cmov {
+    /// Move if zero.
+    ///
+    /// Uses a `cmp` instruction to check if the given `condition` value is
+    /// equal to zero, and if so, conditionally moves `value` to `self`
+    /// when `condition` is equal to zero.
+    fn cmovz(&mut self, value: Self, condition: Condition);
+
+    /// Move if non-zero.
+    ///
+    /// Uses a `test` instruction to check if the given `condition` value is
+    /// equal to zero, conditionally moves `value` to `self` when `condition` is
+    /// equal to zero.
+    fn cmovnz(&mut self, value: Self, condition: Condition);
+}
+
+impl Cmov for u8 {
+    #[inline(always)]
+    fn cmovz(&mut self, value: Self, condition: Condition) {
+        let mut tmp = *self as u16;
+        tmp.cmovz(value as u16, condition);
+        *self = tmp as u8;
+    }
+
+    #[inline(always)]
+    fn cmovnz(&mut self, value: Self, condition: Condition) {
+        let mut tmp = *self as u16;
+        tmp.cmovnz(value as u16, condition);
+        *self = tmp as u8;
     }
 }
 
-/// Move if zero.
-///
-/// Uses a `cmp` instruction to check if the given `condition` value is
-/// equal to zero, then calls `csel` to conditionally move
-/// `src` to `dst` when `condition` is equal to zero.
-#[cfg(any(target_arch = "aarch64"))]
-#[inline(always)]
-pub fn cmovz(condition: usize, src: usize, dst: &mut usize) {
-    unsafe {
-        asm! {
-            "cmp {0}, 0",
-            "csel {1}, {2}, {3}, EQ",
-            in(reg) condition,
-            inlateout(reg) *dst,
-            in(reg) src,
-            in(reg) *dst,
-            options(pure, nomem, nostack),
-        };
-    }
-}
+impl Cmov for u128 {
+    #[inline(always)]
+    fn cmovz(&mut self, value: Self, condition: Condition) {
+        let mut lo = (*self & u64::MAX as u128) as u64;
+        let mut hi = (*self >> 64) as u64;
 
-/// Move if not zero.
-///
-/// Uses a `cmp` instruction to check if the given `condition` value is not
-/// equal to zero, then calls `csel` to conditionally move
-/// `src` to `dst` when `condition` is nonzero.
-#[cfg(any(target_arch = "aarch64"))]
-#[inline(always)]
-pub fn cmovnz(condition: usize, src: usize, dst: &mut usize) {
-    unsafe {
-        asm! {
-            "cmp {0}, 0",
-            "csel {1}, {2}, {3}, NE",
-            in(reg) condition,
-            inlateout(reg) *dst,
-            in(reg) src,
-            in(reg) *dst,
-            options(pure, nomem, nostack),
-        };
-    }
-}
+        lo.cmovz((value & u64::MAX as u128) as u64, condition);
+        hi.cmovz((value >> 64) as u64, condition);
 
-/// Move if not zero.
-///
-/// Uses a `test` instruction to check if the given `condition` value is not
-/// equal to zero, then calls `cmovnz` (a.k.a. `cmovne`) to conditionally move
-/// `src` to `dst` when `condition` is nonzero.
-#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
-#[inline(always)]
-pub fn cmovnz(condition: usize, src: usize, dst: &mut usize) {
-    unsafe {
-        asm! {
-            "test {0}, {0}",
-            "cmovnz {1}, {2}",
-            in(reg) condition,
-            inlateout(reg) *dst,
-            in(reg) src,
-            options(pure, nomem, nostack),
-        };
-    }
-}
-
-/// Move if zero (portable fallback implementation).
-///
-/// This implementation is based on portable bitwise arithmetic but cannot
-/// guarantee that the resulting generated assembly is free of branch
-/// instructions.
-#[cfg(not(any(target_arch = "x86", target_arch = "x86_64", target_arch = "aarch64")))]
-#[inline(never)]
-pub fn cmovz(condition: usize, src: usize, dst: &mut usize) {
-    let mask = (1 ^ is_non_zero(condition)).wrapping_sub(1);
-    *dst = (*dst & mask) | (src & !mask);
-}
-
-/// Move if not zero (portable fallback implementation).
-///
-/// This implementation is based on portable bitwise arithmetic but cannot
-/// guarantee that the resulting generated assembly is free of branch
-/// instructions.
-#[cfg(not(any(target_arch = "x86", target_arch = "x86_64", target_arch = "aarch64")))]
-#[inline(never)]
-pub fn cmovnz(condition: usize, src: usize, dst: &mut usize) {
-    let mask = is_non_zero(condition).wrapping_sub(1);
-    *dst = (*dst & mask) | (src & !mask);
-}
-
-/// Check if the given condition value is non-zero
-///
-/// # Returns
-/// - `condition` is zero: `0`
-/// - `condition` is non-zero: `1`
-#[cfg(not(any(target_arch = "x86", target_arch = "x86_64", target_arch = "aarch64")))]
-#[inline(always)]
-fn is_non_zero(condition: usize) -> usize {
-    const SHIFT_BITS: usize = core::mem::size_of::<usize>() - 1;
-    ((condition | (!condition).wrapping_add(1)) >> SHIFT_BITS) & 1
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn cmovz_works() {
-        let mut n = 24;
-        cmovz(42, 42, &mut n);
-        assert_eq!(n, 24);
-        cmovz(0, 42, &mut n);
-        assert_eq!(n, 42);
+        *self = (lo as u128) | (hi as u128) << 64;
     }
 
-    #[test]
-    fn cmovnz_works() {
-        let mut n = 24;
-        cmovnz(0, 42, &mut n);
-        assert_eq!(n, 24);
-        cmovnz(42, 42, &mut n);
-        assert_eq!(n, 42);
+    #[inline(always)]
+    fn cmovnz(&mut self, value: Self, condition: Condition) {
+        let mut lo = (*self & u64::MAX as u128) as u64;
+        let mut hi = (*self >> 64) as u64;
+
+        lo.cmovnz((value & u64::MAX as u128) as u64, condition);
+        hi.cmovnz((value >> 64) as u64, condition);
+
+        *self = (lo as u128) | (hi as u128) << 64;
     }
 }
