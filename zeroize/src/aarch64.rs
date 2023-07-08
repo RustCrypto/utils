50fd77561bd23728b016e1d66dc5a0de24a543d4
Artyom Pavlov <newpavlov@gmail.com>
2023-07-09 01:05:50 +0300

    zeroize: remove unnecessary cfgs on SIMD type impls (#930)

--- zeroize/src/aarch64.rs
+++ zeroize/src/aarch64.rs
@@ -8,13 +8,13 @@ use crate::{atomic_fence, volatile_write, Zeroize};
 use core::arch::aarch64::*;
 
 macro_rules! impl_zeroize_for_simd_register {
-    ($(($type:ty, $vdupq:ident)),+) => {
+    ($($type:ty),* $(,)?) => {
         $(
             #[cfg_attr(docsrs, doc(cfg(target_arch = "aarch64")))]
-            #[cfg_attr(docsrs, doc(cfg(target_feature = "neon")))]
             impl Zeroize for $type {
+                #[inline]
                 fn zeroize(&mut self) {
-                    volatile_write(self, unsafe { $vdupq(0) });
+                    volatile_write(self, unsafe { core::mem::zeroed() });
                     atomic_fence();
                 }
             }
@@ -24,12 +24,12 @@ macro_rules! impl_zeroize_for_simd_register {
 
 // TODO(tarcieri): other NEON register types?
 impl_zeroize_for_simd_register! {
-    (uint8x8_t, vdup_n_u8),
-    (uint8x16_t, vdupq_n_u8),
-    (uint16x4_t, vdup_n_u16),
-    (uint16x8_t, vdupq_n_u16),
-    (uint32x2_t, vdup_n_u32),
-    (uint32x4_t, vdupq_n_u32),
-    (uint64x1_t, vdup_n_u64),
-    (uint64x2_t, vdupq_n_u64)
+    uint8x8_t,
+    uint8x16_t,
+    uint16x4_t,
+    uint16x8_t,
+    uint32x2_t,
+    uint32x4_t,
+    uint64x1_t,
+    uint64x2_t,
 }
