e4a7e7f8a21ac96ce2d9c8c302b55b22eb95a689
zonyitoo <zonyitoo@gmail.com>
2023-10-26 14:18:24 +0800

    cpufeatures: add support of armv8.4-a sm3, sm4 (#970)
    Only supported on Linux platform with HWCAP_SM3 | HWCAP_SM4.

--- cpufeatures/src/aarch64.rs
+++ cpufeatures/src/aarch64.rs
@@ -69,6 +69,7 @@ __expand_check_macro! {
     ("aes",    AES),    // Enable AES support.
     ("sha2",   SHA2),   // Enable SHA1 and SHA256 support.
     ("sha3",   SHA3),   // Enable SHA512 and SHA3 support.
+    ("sm4",    SM4),    // Enable SM3 and SM4 support.
 }
 
 /// Linux hardware capabilities mapped to target features.
@@ -85,6 +86,7 @@ pub mod hwcaps {
     pub const AES: c_ulong = libc::HWCAP_AES | libc::HWCAP_PMULL;
     pub const SHA2: c_ulong = libc::HWCAP_SHA2;
     pub const SHA3: c_ulong = libc::HWCAP_SHA3 | libc::HWCAP_SHA512;
+    pub const SM4: c_ulong = libc::HWCAP_SM3 | libc::HWCAP_SM4;
 }
 
 // Apple OS (macOS, iOS, watchOS, and tvOS) `check!` macro.
