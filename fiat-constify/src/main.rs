ce47b3c74f46217995192d092c4d2dd3bc954986
Brent Lewis <coder0xff@gmail.com>
2024-03-13 13:57:08 -0700

    fiat-constify: filter out `let mut` declarations (#1057)
    Avoids unused variable warnings

--- fiat-constify/src/main.rs
+++ fiat-constify/src/main.rs
@@ -199,6 +199,21 @@ fn rewrite_fn_body(stmts: &[Stmt], outputs: &Outputs) -> Vec<Stmt> {
             }
         } else if let Stmt::Expr(Expr::Call(expr), Some(_)) = stmt {
             rewritten.push(Stmt::Local(rewrite_fn_call(expr.clone())));
+        } else if let Stmt::Local(Local {
+            pat: Pat::Type(pat),
+            ..
+        }) = stmt
+        {
+            let unboxed = pat.pat.as_ref();
+            if let Pat::Ident(PatIdent {
+                mutability: Some(_),
+                ..
+            }) = unboxed
+            {
+                // This is a mut var, in the case of fiat-crypto transformation dead code
+            } else {
+                rewritten.push(stmt.clone());
+            }
         } else {
             rewritten.push(stmt.clone());
         }
