6d383a554b76df2c6796d8d2982a6a8535342617
Tony Arcieri <bascule@gmail.com>
2024-04-24 08:07:18 -0600

    zeroize: always enable AArch64 support (#1064)
    Now that our MSRV is high enough we don't need to feature-gate `aarch64`
    support anymore, and instead it can be always-on when compiling for
    `aarch64` targets.
    
    The `aarch64` feature is retained as a noop since removing it would be a
    breaking change.

--- zeroize/src/lib.rs
+++ zeroize/src/lib.rs
@@ -245,7 +245,7 @@ extern crate std;
 #[cfg(feature = "zeroize_derive")]
 pub use zeroize_derive::{Zeroize, ZeroizeOnDrop};
 
-#[cfg(all(feature = "aarch64", target_arch = "aarch64"))]
+#[cfg(target_arch = "aarch64")]
 mod aarch64;
 #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
 mod x86;
