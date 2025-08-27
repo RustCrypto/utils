165ea6c810c77f0707ae110a38297e65d9df4ecb
Артём Павлов [Artyom Pavlov] <newpavlov@gmail.com>
2025-08-27 14:55:06 +0300

    Tweak test

--- blobby/tests/mod.rs
+++ blobby/tests/mod.rs
@@ -1,6 +1,8 @@
 #![cfg(feature = "alloc")]
 
-const TEST_BLOBS: &[&[u8]] = &[
+const ITEMS_LEN: usize = 10;
+const DEDUP_LEN: usize = 3;
+const TEST_BLOBS: &[&[u8]; ITEMS_LEN] = &[
     b"1",
     b"12",
     b"1",
@@ -8,20 +10,22 @@ const TEST_BLOBS: &[&[u8]] = &[
     b"123",
     &[42; 100_000],
     &[42; 100_000],
-    &[13; 70_000],
+    &[13; 7_000],
+    &[13; 7_000],
+    &[13; 5_000],
 ];
 
 #[test]
 fn blobby_rondtrip_test() -> Result<(), blobby::Error> {
     let (blobby_data, dedup_len) = blobby::encode_blobs(TEST_BLOBS);
-    assert_eq!(dedup_len, 2);
-    assert_eq!(blobby_data.len(), 170_022);
+    assert_eq!(dedup_len, DEDUP_LEN);
+    assert_eq!(blobby_data.len(), 112_025);
 
-    let decoded_blobs = blobby::parse_into_array::<8, 2>(&blobby_data)?;
-    assert_eq!(decoded_blobs, TEST_BLOBS);
+    let decoded_blobs = blobby::parse_into_array::<ITEMS_LEN, DEDUP_LEN>(&blobby_data)?;
+    assert_eq!(decoded_blobs, TEST_BLOBS[..]);
 
     let decoded_blobs = blobby::parse_into_vec(&blobby_data)?;
-    assert_eq!(decoded_blobs, TEST_BLOBS);
+    assert_eq!(decoded_blobs, TEST_BLOBS[..]);
 
     Ok(())
 }
