7f0ea63c158626632291589b066348fec7df9cb6
Артём Павлов [Artyom Pavlov] <newpavlov@gmail.com>
2025-09-21 10:15:38 +0300

    block-buffer: improve `split_blocks` implementations

--- block-buffer/src/sealed.rs
+++ block-buffer/src/sealed.rs
@@ -1,7 +1,7 @@
 use hybrid_array::sizes::{U0, U1};
 
 use super::{Array, ArraySize};
-use core::{mem::MaybeUninit, ptr, slice};
+use core::{mem::MaybeUninit, ptr};
 
 type Block<N> = MaybeUninit<Array<u8, N>>;
 
@@ -60,18 +60,7 @@ impl Sealed for super::Eager {
 
     #[inline(always)]
     fn split_blocks<N: ArraySize>(data: &[u8]) -> (&[Array<u8, N>], &[u8]) {
-        let nb = data.len() / N::USIZE;
-        let blocks_len = nb * N::USIZE;
-        let tail_len = data.len() - blocks_len;
-        // SAFETY: we guarantee that created slices do not point outside of `data`
-        unsafe {
-            let blocks_ptr = data.as_ptr() as *const Array<u8, N>;
-            let tail_ptr = data.as_ptr().add(blocks_len);
-            (
-                slice::from_raw_parts(blocks_ptr, nb),
-                slice::from_raw_parts(tail_ptr, tail_len),
-            )
-        }
+        Array::slice_as_chunks(data)
     }
 }
 
@@ -96,24 +85,12 @@ impl Sealed for super::Lazy {
 
     #[inline(always)]
     fn split_blocks<N: ArraySize>(data: &[u8]) -> (&[Array<u8, N>], &[u8]) {
-        if data.is_empty() {
-            return (&[], &[]);
-        }
-        let (nb, tail_len) = if data.len() % N::USIZE == 0 {
-            (data.len() / N::USIZE - 1, N::USIZE)
+        let (blocks, tail) = Array::slice_as_chunks(data);
+        if data.is_empty() || !tail.is_empty() {
+            (blocks, tail)
         } else {
-            let nb = data.len() / N::USIZE;
-            (nb, data.len() - nb * N::USIZE)
-        };
-        let blocks_len = nb * N::USIZE;
-        // SAFETY: we guarantee that created slices do not point outside of `data`
-        unsafe {
-            let blocks_ptr = data.as_ptr() as *const Array<u8, N>;
-            let tail_ptr = data.as_ptr().add(blocks_len);
-            (
-                slice::from_raw_parts(blocks_ptr, nb),
-                slice::from_raw_parts(tail_ptr, tail_len),
-            )
+            let (tail, blocks) = blocks.split_last().expect("`blocks` can not be empty");
+            (blocks, tail)
         }
     }
 }
