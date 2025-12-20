14297e2ab2d717149f80730f9b52a31179ee449f
Tony Arcieri <bascule@gmail.com>
2025-12-19 19:56:15 -0700

    ctutils: add `CtGt` and `CtLt` (#1264)
    Adds traits for computing greater than/less than in constant time.
    
    Uses overflowing subtraction as the implementation strategy for unsigned
    integers.
    
    Also adds impls for `cmp::Ordering`.

--- ctutils/src/lib.rs
+++ ctutils/src/lib.rs
@@ -70,4 +70,4 @@ mod traits;
 
 pub use choice::Choice;
 pub use ct_option::CtOption;
-pub use traits::{ct_eq::CtEq, ct_select::CtSelect};
+pub use traits::{ct_eq::CtEq, ct_gt::CtGt, ct_lt::CtLt, ct_select::CtSelect};
