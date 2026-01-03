df6e1b3e8750589056f2a1ded40063308c13a4d5
Tony Arcieri <bascule@gmail.com>
2026-01-03 13:33:26 -0700

    ctutils: initial proptests (#1327)
    Adds initial proptests for `CtEq::ct_eq` and `CtSelect::ct_select` for
    all core signed and unsigned integer types

---
+++ ctutils/tests/proptests.rs
@@ -0,0 +1,56 @@
+/// Write the proptests for an integer type.
+macro_rules! int_proptests {
+    ( $($int:ident),+ ) => {
+        $(
+            mod $int {
+                use ctutils::{CtSelect, CtEq, Choice};
+                use proptest::prelude::*;
+
+                proptest! {
+                    #[test]
+                    fn ct_assign(a in any::<$int>(), b in any::<$int>(), byte in any::<u8>()) {
+                        let choice = Choice::from_u8_lsb(byte);
+                        let mut actual = a;
+                        actual.ct_assign(&b, choice);
+
+                        let expected = if byte & 1 == 1 {
+                            b
+                        } else {
+                            a
+                        };
+
+                        prop_assert_eq!(expected, actual);
+                    }
+
+                    #[test]
+                     fn ct_eq(a in any::<$int>(), b in any::<$int>()) {
+                        let actual = a.ct_eq(&b);
+                        prop_assert_eq!(a == b, actual.to_bool());
+                     }
+
+                     #[test]
+                     fn ct_ne(a in any::<$int>(), b in any::<$int>()) {
+                        let actual = a.ct_ne(&b);
+                        prop_assert_eq!(a != b, actual.to_bool());
+                     }
+
+                    #[test]
+                    fn ct_select(a in any::<$int>(), b in any::<$int>(), byte in any::<u8>()) {
+                        let choice = Choice::from_u8_lsb(byte);
+                        let actual = a.ct_select(&b, choice);
+
+                        let expected = if byte & 1 == 1 {
+                            b
+                        } else {
+                            a
+                        };
+
+                        prop_assert_eq!(expected, actual);
+                    }
+                }
+            }
+        )+
+    };
+}
+
+int_proptests!(i8, i16, i32, i64, i128, u8, u16, u32, u64, u128);
