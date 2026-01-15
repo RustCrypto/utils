4bb07d1c4352f47323f52defdce17259b0d71f99
Tony Arcieri <bascule@gmail.com>
2026-01-15 14:37:02 -0700

    cmov: impl optimized `Cmov` for `[u8; N]` (#1350)
    Adds a specialized impl of `Cmov` for byte arrays of generic size which
    first coalesces elements of the array into word-sized chunks, then calls
    `Cmov` on those.
    
    This should result in significantly more efficient codegen, which can
    also take advantage of compile-time knowledge of `N` to potentially
    unroll loops.
    
    Unfortunately without specialization this means we can't impl `Cmov` for
    other types of arrays, but downstream consumers can just iterate over
    them and call `Cmov::cmov*` on each element easily enough, whereas this
    optimized implementation for byte arrays actually provides something a
    lot less trivial than looping over an array.

--- cmov/tests/proptests.rs
+++ cmov/tests/proptests.rs
@@ -9,55 +9,51 @@ macro_rules! int_proptests {
                 proptest! {
                     #[test]
                     fn cmovz_works(mut a in any::<$int>(), b in any::<$int>(), cond in any::<u8>()) {
-                        a.cmovz(&b, cond);
-
                         let expected = if cond == 0 {
                             b
                         } else {
                             a
                         };
 
+                        a.cmovz(&b, cond);
                         prop_assert_eq!(expected, a);
                     }
 
                     #[test]
                     fn cmovnz_works(mut a in any::<$int>(), b in any::<$int>(), cond in any::<u8>()) {
-                        a.cmovnz(&b, cond);
-
                         let expected = if cond != 0 {
                             b
                         } else {
                             a
                         };
 
+                        a.cmovnz(&b, cond);
                         prop_assert_eq!(expected, a);
                     }
 
                     #[test]
                      fn cmoveq_works(a in any::<$int>(), b in any::<$int>(), cond in any::<u8>()) {
-                        let mut actual = 0;
-                        a.cmoveq(&b, cond, &mut actual);
-
                         let expected = if a == b {
                             cond
                         } else {
                             0
                         };
 
+                        let mut actual = 0;
+                        a.cmoveq(&b, cond, &mut actual);
                         prop_assert_eq!(expected, actual);
                      }
 
                     #[test]
                      fn cmovne_works(a in any::<$int>(), b in any::<$int>(), cond in any::<u8>()) {
-                        let mut actual = 0;
-                        a.cmovne(&b, cond, &mut actual);
-
                         let expected = if a != b {
                             cond
                         } else {
                             0
                         };
 
+                        let mut actual = 0;
+                        a.cmovne(&b, cond, &mut actual);
                         prop_assert_eq!(expected, actual);
                      }
                 }
@@ -66,4 +62,61 @@ macro_rules! int_proptests {
     };
 }
 
+/// Write the proptests for a byte array of the given size.
+macro_rules! byte_array_proptests {
+    ( $($name:ident: $size:expr),+ ) => {
+        $(
+            mod $name {
+                use cmov::Cmov;
+                use proptest::prelude::*;
+
+                proptest! {
+                    #[test]
+                    fn cmovnz_works(
+                        mut a in any::<[u8; $size]>(),
+                        b in any::<[u8; $size]>(),
+                        cond in any::<u8>()
+                    ) {
+                        let expected = if cond == 0 {
+                            a
+                        } else {
+                            b
+                        };
+
+                        a.cmovnz(&b, cond);
+                        prop_assert_eq!(expected, a);
+                    }
+                }
+            }
+        )+
+    };
+}
+
 int_proptests!(i8, i16, i32, i64, i128, u8, u16, u32, u64, u128);
+byte_array_proptests!(
+    array0: 0,
+    array1: 1,
+    array2: 2,
+    array3: 3,
+    array4: 4,
+    array5: 5,
+    array6: 6,
+    array7: 7,
+    array8: 8,
+    array9: 9,
+    array10: 10,
+    array11: 11,
+    array12: 12,
+    array13: 13,
+    array14: 14,
+    array15: 15,
+    array16: 16,
+    array17: 17,
+    array18: 18,
+    array19: 19,
+    array20: 20,
+    array21: 21,
+    array22: 22,
+    array23: 23,
+    array24: 24
+);
