9b92270c2948906bc2ce687e4a080f59210c2808
Артём Павлов [Artyom Pavlov] <newpavlov@gmail.com>
2026-05-04 16:29:05 +0300

    Switch rate to const generics

--- sponge-cursor/src/lib.rs
+++ sponge-cursor/src/lib.rs
@@ -5,10 +5,7 @@
     html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/media/6ee8e381/logo.svg"
 )]
 
-pub use hybrid_array as array;
-
-use core::marker::PhantomData;
-use hybrid_array::{Array, ArraySize};
+mod utils;
 
 /// Cursor for implementing sponge-based absorption and squeezing.
 ///
@@ -17,36 +14,31 @@ use hybrid_array::{Array, ArraySize};
 /// `Rate` MUST be smaller than `U256`, trying to initialize cursor with an invalid rate will
 /// result in a compilation error.
 #[derive(Debug, Clone)]
-pub struct SpongeCursor<Rate: ArraySize> {
+pub struct SpongeCursor<const RATE: usize> {
     pos: u8,
-    _pd: PhantomData<Rate>,
 }
 
-impl<Rate: ArraySize> Default for SpongeCursor<Rate> {
+impl<const RATE: usize> Default for SpongeCursor<RATE> {
     fn default() -> Self {
         const {
-            assert!(Rate::USIZE != 0);
-            assert!(Rate::USIZE < u8::MAX as usize);
+            assert!(RATE != 0);
+            assert!(RATE < u8::MAX as usize);
         }
 
-        Self {
-            pos: 0,
-            _pd: PhantomData,
-        }
+        Self { pos: 0 }
     }
 }
 
-impl<Rate: ArraySize> SpongeCursor<Rate> {
+// Note that the methods should compile into a panic-free code,
+// see: https://rust.godbolt.org/z/3v4zWr4ox
+impl<const RATE: usize> SpongeCursor<RATE> {
     /// Create new cursor with the provided position.
     ///
     /// Returns `None` if `pos` is bigger or equal to `Rate`.
     #[must_use]
     pub fn new(pos: u8) -> Option<Self> {
-        if usize::from(pos) < Rate::USIZE {
-            Some(Self {
-                pos,
-                _pd: PhantomData,
-            })
+        if usize::from(pos) < RATE {
+            Some(Self { pos })
         } else {
             None
         }
@@ -55,9 +47,11 @@ impl<Rate: ArraySize> SpongeCursor<Rate> {
     /// Get current cursor position as `u8`.
     #[must_use]
     #[inline(always)]
+    #[allow(clippy::missing_panics_doc, reason = "the method is panic-free")]
     pub fn raw_pos(&self) -> u8 {
-        debug_assert!(self.pos < Rate::U8);
-        if self.pos < Rate::U8 {
+        let rate_u8 = u8::try_from(RATE).expect("RATE is smaller than 256");
+        debug_assert!(self.pos < rate_u8);
+        if self.pos < rate_u8 {
             self.pos
         } else {
             // SAFETY: the type enforces that `pos` is always smaller than `Rate`
@@ -65,13 +59,13 @@ impl<Rate: ArraySize> SpongeCursor<Rate> {
         }
     }
 
-    /// Get current cursor position.
+    /// Get current cursor position as `usize`.
     #[must_use]
     #[inline(always)]
     pub fn pos(&self) -> usize {
         let pos = usize::from(self.pos);
-        debug_assert!(pos < Rate::USIZE);
-        if pos < Rate::USIZE {
+        debug_assert!(pos < RATE);
+        if pos < RATE {
             pos
         } else {
             // SAFETY: the type enforces that `pos` is always smaller than `Rate`
@@ -85,7 +79,7 @@ impl<Rate: ArraySize> SpongeCursor<Rate> {
     /// a compilation error.
     #[allow(
         clippy::missing_panics_doc,
-        reason = "the function is panic-free, see: https://rust.godbolt.org/z/WdordTqvv"
+        reason = "the method is panic-free, see: https://rust.godbolt.org/z/88Wf1qEsr"
     )]
     #[inline]
     pub fn absorb_u64_le<const N: usize>(
@@ -95,14 +89,14 @@ impl<Rate: ArraySize> SpongeCursor<Rate> {
         mut data: &[u8],
     ) {
         const {
-            assert!(Rate::USIZE <= size_of::<[u64; N]>());
-            assert!(Rate::USIZE < u8::MAX as usize);
-            assert!(Rate::USIZE % size_of::<u64>() == 0);
+            assert!(RATE <= size_of::<[u64; N]>());
+            assert!(RATE < u8::MAX as usize);
+            assert!(RATE % size_of::<u64>() == 0);
         };
 
         if self.pos != 0 {
             let pos = self.pos();
-            let rem_len = Rate::USIZE - pos;
+            let rem_len = RATE - pos;
 
             let is_partial = data.len() < rem_len;
             let head = if is_partial {
@@ -113,7 +107,7 @@ impl<Rate: ArraySize> SpongeCursor<Rate> {
                 head
             };
 
-            xor_into::<N, Rate>(state, pos, head);
+            utils::xor_into_state::<N, RATE>(state, pos, head);
 
             if is_partial {
                 self.pos = u8::try_from(pos + head.len()).expect("the sum is smaller than Rate");
@@ -123,7 +117,7 @@ impl<Rate: ArraySize> SpongeCursor<Rate> {
             sponge(state);
         }
 
-        let blocks = data.chunks_exact(Rate::USIZE);
+        let blocks = data.chunks_exact(RATE);
         let tail = blocks.remainder();
 
         for block in blocks {
@@ -139,126 +133,96 @@ impl<Rate: ArraySize> SpongeCursor<Rate> {
         }
 
         if !tail.is_empty() {
-            xor_into::<N, Rate>(state, 0, tail);
+            utils::xor_into_state::<N, RATE>(state, 0, tail);
         }
 
-        self.pos = u8::try_from(tail.len()).expect("tail.len() is smaller than Rate");
+        self.pos = u8::try_from(tail.len()).expect("tail.len() is smaller than RATE");
     }
 
-    /// Squeeze data into `dst`.
-    #[allow(clippy::missing_panics_doc, reason = "the function is panic-free")]
-    pub fn squeeze_u64_le<const N: usize>(
+    /// Squeeze data by reading it into `buf`.
+    pub fn squeeze_read_u64_le<const N: usize>(
         &mut self,
         state: &mut [u64; N],
         sponge: fn(&mut [u64; N]),
-        mut dst: &mut [u8],
+        buf: &mut [u8],
+    ) {
+        self.squeeze_inner_u64_le(
+            state,
+            sponge,
+            buf,
+            utils::squeeze_read_partial::<N, RATE>,
+            utils::squeeze_read_full,
+        );
+    }
+
+    /// Squeeze data by XOR-ing it with data in `buf`.
+    pub fn squeeze_xor_u64_le<const N: usize>(
+        &mut self,
+        state: &mut [u64; N],
+        sponge: fn(&mut [u64; N]),
+        buf: &mut [u8],
+    ) {
+        self.squeeze_inner_u64_le(
+            state,
+            sponge,
+            buf,
+            utils::squeeze_xor_partial::<N, RATE>,
+            utils::squeeze_xor_full,
+        );
+    }
+
+    fn squeeze_inner_u64_le<const N: usize>(
+        &mut self,
+        state: &mut [u64; N],
+        sponge: fn(&mut [u64; N]),
+        mut buf: &mut [u8],
+        process_partial: fn(&[u64; N], usize, &mut [u8]),
+        process_full: fn(&[u64; N], &mut [u8; RATE]),
     ) {
         const {
-            assert!(Rate::USIZE <= size_of::<[u64; N]>());
-            assert!(Rate::USIZE < u8::MAX as usize);
-            assert!(Rate::USIZE % size_of::<u64>() == 0);
+            assert!(RATE <= size_of::<[u64; N]>());
+            assert!(RATE < u8::MAX as usize);
+            assert!(RATE % size_of::<u64>() == 0);
         };
 
         if self.pos != 0 {
             let pos = self.pos();
-            let rem_len = Rate::USIZE - pos;
+            let rem_len = RATE - pos;
 
-            let is_partial = dst.len() < rem_len;
-            let split_pos = if is_partial { dst.len() } else { rem_len };
-
-            let (head, tail) = dst.split_at_mut(split_pos);
-            dst = tail;
-
-            read_into::<N, Rate>(state, pos, head);
-
-            if is_partial {
-                self.pos = u8::try_from(pos + head.len()).expect("the sum is smaller than Rate");
+            if buf.len() < rem_len {
+                process_partial(state, pos, buf);
+                self.pos = u8::try_from(pos + buf.len()).expect("the sum is smaller than RATE");
                 return;
             }
+
+            let (head, tail) = buf.split_at_mut(rem_len);
+            buf = tail;
+
+            process_partial(state, pos, head);
         }
 
-        let mut blocks = dst.chunks_exact_mut(Rate::USIZE);
+        let mut blocks = buf.chunks_exact_mut(RATE);
 
         for block in &mut blocks {
             sponge(state);
-
-            let mut dst_chunks = block.chunks_exact_mut(size_of::<u64>());
-            for (src, dst_chunk) in state.iter_mut().zip(&mut dst_chunks) {
-                dst_chunk.copy_from_slice(&src.to_le_bytes());
-            }
-            assert!(dst_chunks.into_remainder().is_empty());
+            let block = block.try_into().expect("block has correct size");
+            process_full(state, block);
         }
 
         let tail = blocks.into_remainder();
 
         if !tail.is_empty() {
             sponge(state);
-            read_into::<N, Rate>(state, 0, tail);
+            process_partial(state, 0, tail);
         }
 
-        self.pos = u8::try_from(tail.len()).expect("tail.len() is smaller than Rate");
+        self.pos = u8::try_from(tail.len()).expect("tail.len() is smaller than RATE");
     }
 }
 
 #[cfg(feature = "zeroize")]
-impl<Rate: ArraySize> zeroize::Zeroize for SpongeCursor<Rate> {
+impl<const RATE: usize> zeroize::Zeroize for SpongeCursor<RATE> {
     fn zeroize(&mut self) {
         self.pos.zeroize();
-    }
-}
-
-#[inline(always)]
-fn xor_into<const N: usize, Rate: ArraySize>(state: &mut [u64; N], offset: usize, data: &[u8]) {
-    const {
-        assert!(size_of::<Array<u8, Rate>>() <= size_of::<[u64; N]>());
-        assert!(Rate::USIZE % size_of::<u64>() == 0);
-    };
-
-    if cfg!(target_endian = "little") {
-        // SAFETY: casting of `&mut [u64; N]` into `&mut [u8; M]` is safe if
-        // `size_of::<[u8; M]>() <= size_of::<[u64; N]>())`,
-        // the same applies to `Array` since it's a transparent wrapper around `[T; N]`
-        let sub_state: &mut Array<u8, Rate> = unsafe { &mut *(state.as_mut_ptr().cast()) };
-
-        let dst = &mut sub_state[offset..][..data.len()];
-
-        for i in 0..dst.len() {
-            dst[i] ^= data[i];
-        }
-    } else {
-        let mut buf = Array::<u8, Rate>::default();
-        buf[offset..][..data.len()].copy_from_slice(data);
-
-        let chunks = buf.chunks_exact(size_of::<u64>());
-        assert!(chunks.remainder().is_empty());
-
-        for (dst, chunk) in state.iter_mut().zip(chunks) {
-            let chunk = chunk.try_into().expect("chunk has correct length");
-            *dst ^= u64::from_le_bytes(chunk);
-        }
-    }
-}
-
-#[inline(always)]
-fn read_into<const N: usize, Rate: ArraySize>(state: &[u64; N], offset: usize, dst: &mut [u8]) {
-    const {
-        assert!(size_of::<Array<u8, Rate>>() <= size_of::<[u64; N]>());
-        assert!(Rate::USIZE % size_of::<u64>() == 0);
-    };
-
-    if cfg!(target_endian = "little") {
-        // SAFETY: casting of `&mut [u64; N]` into `&mut [u8; M]` is safe if
-        // `size_of::<[u8; M]>() <= size_of::<[u64; N]>())`,
-        // the same applies to `Array` since it's a transparent wrapper around `[T; N]`
-        let sub_state: &Array<u8, Rate> = unsafe { &*(state.as_ptr().cast()) };
-        dst.copy_from_slice(&sub_state[offset..][..dst.len()]);
-    } else {
-        let mut buf = Array::<u8, Rate>::default();
-        let mut chunks = buf.chunks_exact_mut(size_of::<u64>());
-        for (src, dst) in state.iter().zip(&mut chunks) {
-            dst.copy_from_slice(&src.to_le_bytes());
-        }
-        assert!(chunks.into_remainder().is_empty());
-        dst.copy_from_slice(&buf[offset..][..dst.len()]);
     }
 }
---
+++ sponge-cursor/src/utils.rs
@@ -0,0 +1,136 @@
+#[inline(always)]
+pub(crate) fn xor_into_state<const N: usize, const RATE: usize>(
+    state: &mut [u64; N],
+    offset: usize,
+    data: &[u8],
+) {
+    const {
+        assert!(size_of::<[u8; RATE]>() <= size_of::<[u64; N]>());
+        assert!(RATE % size_of::<u64>() == 0);
+    };
+
+    if cfg!(target_endian = "little") {
+        // SAFETY: casting of `&mut [u64; N]` into `&mut [u8; M]` is safe if
+        // `size_of::<[u8; M]>() <= size_of::<[u64; N]>())`
+        let sub_state: &mut [u8; RATE] = unsafe { &mut *(state.as_mut_ptr().cast()) };
+
+        let dst = &mut sub_state[offset..][..data.len()];
+
+        for i in 0..dst.len() {
+            dst[i] ^= data[i];
+        }
+    } else {
+        let mut buf = [0u8; RATE];
+        buf[offset..][..data.len()].copy_from_slice(data);
+
+        let chunks = buf.chunks_exact(size_of::<u64>());
+        assert!(chunks.remainder().is_empty());
+
+        for (dst, chunk) in state.iter_mut().zip(chunks) {
+            let chunk = chunk.try_into().expect("chunk has correct length");
+            *dst ^= u64::from_le_bytes(chunk);
+        }
+    }
+}
+
+#[inline(always)]
+pub(crate) fn squeeze_read_full<const N: usize, const RATE: usize>(
+    state: &[u64; N],
+    dst: &mut [u8; RATE],
+) {
+    const {
+        assert!(size_of::<[u8; RATE]>() <= size_of::<[u64; N]>());
+        assert!(RATE % size_of::<u64>() == 0);
+    };
+
+    let mut dst_chunks = dst.chunks_exact_mut(size_of::<u64>());
+    for (src, dst_chunk) in state.iter().zip(&mut dst_chunks) {
+        dst_chunk.copy_from_slice(&src.to_le_bytes());
+    }
+    assert!(dst_chunks.into_remainder().is_empty());
+}
+
+#[inline(always)]
+pub(crate) fn squeeze_read_partial<const N: usize, const RATE: usize>(
+    state: &[u64; N],
+    offset: usize,
+    dst: &mut [u8],
+) {
+    const {
+        assert!(size_of::<[u8; RATE]>() <= size_of::<[u64; N]>());
+        assert!(RATE % size_of::<u64>() == 0);
+    };
+
+    let (sub_state, mut buf);
+    if cfg!(target_endian = "little") {
+        // SAFETY: casting of `&[u64; N]` into `&[u8; M]` is safe if
+        // `size_of::<[u8; M]>() <= size_of::<[u64; N]>())`
+        sub_state = unsafe { &*(state.as_ptr().cast::<[u8; RATE]>()) };
+    } else {
+        buf = [0u8; RATE];
+
+        let mut chunks = buf.chunks_exact_mut(size_of::<u64>());
+        for (src, dst) in state.iter().zip(&mut chunks) {
+            dst.copy_from_slice(&src.to_le_bytes());
+        }
+        assert!(chunks.into_remainder().is_empty());
+
+        sub_state = &buf;
+    }
+
+    let src = &sub_state[offset..][..dst.len()];
+    dst.copy_from_slice(src);
+}
+
+#[inline(always)]
+pub(crate) fn squeeze_xor_full<const N: usize, const RATE: usize>(
+    state: &[u64; N],
+    dst: &mut [u8; RATE],
+) {
+    const {
+        assert!(size_of::<[u8; RATE]>() <= size_of::<[u64; N]>());
+        assert!(RATE % size_of::<u64>() == 0);
+    };
+
+    let mut dst_chunks = dst.chunks_exact_mut(size_of::<u64>());
+    for (src, dst_chunk) in state.iter().zip(&mut dst_chunks) {
+        let dst_chunk: &mut [u8; 8] = dst_chunk.try_into().expect("chunk has correct size");
+        let chunk_val = u64::from_le_bytes(*dst_chunk);
+        *dst_chunk = (chunk_val ^ src).to_le_bytes();
+    }
+    assert!(dst_chunks.into_remainder().is_empty());
+}
+
+#[inline(always)]
+pub(crate) fn squeeze_xor_partial<const N: usize, const RATE: usize>(
+    state: &[u64; N],
+    offset: usize,
+    dst: &mut [u8],
+) {
+    const {
+        assert!(size_of::<[u8; RATE]>() <= size_of::<[u64; N]>());
+        assert!(RATE % size_of::<u64>() == 0);
+    };
+
+    let (sub_state, mut buf);
+    if cfg!(target_endian = "little") {
+        // SAFETY: casting of `&[u64; N]` into `&[u8; M]` is safe if
+        // `size_of::<[u8; M]>() <= size_of::<[u64; N]>())`
+        sub_state = unsafe { &*(state.as_ptr().cast::<[u8; RATE]>()) };
+    } else {
+        buf = [0u8; RATE];
+
+        let mut chunks = buf.chunks_exact_mut(size_of::<u64>());
+        for (src, dst) in state.iter().zip(&mut chunks) {
+            dst.copy_from_slice(&src.to_le_bytes());
+        }
+        assert!(chunks.into_remainder().is_empty());
+
+        sub_state = &buf;
+    }
+
+    let src = &sub_state[offset..][..dst.len()];
+    for i in 0..dst.len() {
+        dst[i] ^= src[i];
+    }
+}
